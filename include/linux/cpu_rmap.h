3b7817689d0cdbb9cee9f6d5e99cb1c7d1840c35
Vladimir Sokolovsky <vlad@mellanox.com>
2012-12-02 12:57:56 +0200

    BACKPORT: Fix cpu_rmap.h for kernels >= 3.6
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.com>

--- include/linux/cpu_rmap.h
+++ include/linux/cpu_rmap.h
@@ -3,10 +3,8 @@
 
 #include <linux/version.h>
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(3,6,0))
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39))
 #include_next <linux/cpu_rmap.h>
 #endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,39)) */
-#endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,6,0)) */
 
 #endif	/* _COMPAT_LINUX_CPU_RMAP_H */
