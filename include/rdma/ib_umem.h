51a7db63cfd46be77fa02f8ef30ac82e5b62d42d
Alaa Hleihel <alaa@mellanox.com>
2015-01-25 15:26:28 +0200

    ib_core: Extending umem to support contiguous memory operations.
    New structure 'cmem' represents the contiguous allocated memory.
    Allocate, Free and 'Map to virtual address' operations.
    Cleanup mechanism.
    
    contig pages
    
    issue: 493425
    Change-Id: I3d7a6bca7f40e3295d5d1f9a2f896342646556da
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>

--- include/rdma/ib_umem.h
+++ include/rdma/ib_umem.h
@@ -103,6 +103,37 @@ static inline size_t ib_umem_num_pages(struct ib_umem *umem)
 	return (ib_umem_end(umem) - ib_umem_start(umem)) >> PAGE_SHIFT;
 }
 
+/* contiguous memory structure */
+struct ib_cmem {
+
+	struct ib_ucontext     *context;
+	size_t			length;
+	/* Link list of contiguous blocks being part of that cmem  */
+	struct list_head ib_cmem_block;
+
+	/* Order of cmem block,  2^ block_order will equal number
+	     of physical pages per block
+	*/
+	unsigned long    block_order;
+	/* Refernce counter for that memory area
+	  - When value became 0 pages will be returned to the kernel.
+	*/
+	struct kref refcount;
+};
+
+
+struct ib_cmem_block {
+
+	struct list_head	list;
+	/* page will point to the page struct of the head page
+	    in the current compound page.
+	    block order is saved once as part of ib_cmem.
+	*/
+	struct page            *page;
+};
+
+
+
 #ifdef CONFIG_INFINIBAND_USER_MEM
 
 struct ib_umem *ib_umem_get(struct ib_ucontext *context, unsigned long addr,
@@ -117,6 +148,13 @@ int ib_umem_copy_from(void *dst, struct ib_umem *umem, size_t offset,
 
 int ib_umem_map_to_vma(struct ib_umem *umem,
 				struct vm_area_struct *vma);
+int ib_cmem_map_contiguous_pages_to_vma(struct ib_cmem *ib_cmem,
+	struct vm_area_struct *vma);
+struct ib_cmem *ib_cmem_alloc_contiguous_pages(struct ib_ucontext *context,
+				unsigned long total_size,
+				unsigned long page_size_order);
+void ib_cmem_release_contiguous_pages(struct ib_cmem *cmem);
+
 #else /* CONFIG_INFINIBAND_USER_MEM */
 
 #include <linux/err.h>
