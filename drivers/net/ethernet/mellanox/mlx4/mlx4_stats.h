d69cc4255358a0d337bbd8c4cbac24f85143c535
Eran Ben Elisha <eranbe@mellanox.com>
2015-01-26 13:45:20 +0200

    net/mlx4_en: Moderate ethtool to show more statistics [extending_counters]
    More packet statistics are now calculated and visible to the user via
    ethtool.
    
    Issue:418563
    Issue:339647
    Change-Id: I0744da9e1d9f787eef7ee9bc572e266239a1a575
    Signed-off-by: Tal Alon <talal@mellanox.com>
    Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx4/mlx4_stats.h
+++ drivers/net/ethernet/mellanox/mlx4/mlx4_stats.h
@@ -11,24 +11,59 @@
 #define NUM_PRIORITY_STATS 2
 
 struct mlx4_en_pkt_stats {
-	unsigned long broadcast;
-	unsigned long rx_prio[8];
-	unsigned long tx_prio[8];
-#define NUM_PKT_STATS                17
+	unsigned long rx_packets;
+	unsigned long rx_bytes;
+	unsigned long rx_multicast_packets;
+	unsigned long rx_broadcast_packets;
+	unsigned long rx_errors;
+	unsigned long rx_dropped;
+	unsigned long rx_length_errors;
+	unsigned long rx_over_errors;
+	unsigned long rx_crc_errors;
+	unsigned long rx_jabbers;
+	unsigned long rx_in_range_length_error;
+	unsigned long rx_out_range_length_error;
+	unsigned long rx_lt_64_bytes_packets;
+	unsigned long rx_127_bytes_packets;
+	unsigned long rx_255_bytes_packets;
+	unsigned long rx_511_bytes_packets;
+	unsigned long rx_1023_bytes_packets;
+	unsigned long rx_1518_bytes_packets;
+	unsigned long rx_1522_bytes_packets;
+	unsigned long rx_1548_bytes_packets;
+	unsigned long rx_gt_1548_bytes_packets;
+	unsigned long tx_packets;
+	unsigned long tx_bytes;
+	unsigned long tx_multicast_packets;
+	unsigned long tx_broadcast_packets;
+	unsigned long tx_errors;
+	unsigned long tx_dropped;
+	unsigned long tx_lt_64_bytes_packets;
+	unsigned long tx_127_bytes_packets;
+	unsigned long tx_255_bytes_packets;
+	unsigned long tx_511_bytes_packets;
+	unsigned long tx_1023_bytes_packets;
+	unsigned long tx_1518_bytes_packets;
+	unsigned long tx_1522_bytes_packets;
+	unsigned long tx_1548_bytes_packets;
+	unsigned long tx_gt_1548_bytes_packets;
+	unsigned long rx_prio[NUM_PRIORITIES][NUM_PRIORITY_STATS];
+	unsigned long tx_prio[NUM_PRIORITIES][NUM_PRIORITY_STATS];
+#define NUM_PKT_STATS		72
 };
 
 struct mlx4_en_port_stats {
-	unsigned long tso_packets;
+	unsigned long tx_tso_packets;
 	unsigned long xmit_more;
-	unsigned long queue_stopped;
-	unsigned long wake_queue;
+	unsigned long tx_queue_stopped;
+	unsigned long tx_wake_queue;
 	unsigned long tx_timeout;
 	unsigned long rx_alloc_failed;
 	unsigned long rx_chksum_good;
 	unsigned long rx_chksum_none;
 	unsigned long rx_chksum_complete;
 	unsigned long tx_chksum_offload;
-#define NUM_PORT_STATS		9
+#define NUM_PORT_STATS		10
 };
 
 struct mlx4_en_perf_stats {
