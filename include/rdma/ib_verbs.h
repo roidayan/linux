96db363898d6864b8531fdfddef8b761fc5cdd48
Matan Barak <matanb@mellanox.com>
2015-01-27 17:48:53 +0200

    IB/core: Modify ib_verbs and cma in order to use roce_gid_cache
    Previously, we resolved the dmac and took the smac and vlan
    from the resolved address. Changing that into finding a net
    device that matches the IP and vlan of the network packet
    and querying the RoCE GID cache for this net device,
    GID and GID type.
    
    ocrdma driver changes were done by Somnath Kotur <Somnath.Kotur@Emulex.Com>
    
    Change-Id: I3674aa55dc207d0b8334c5800610f3d2aca70988
    Signed-off-by: Matan Barak <matanb@mellanox.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -74,6 +74,8 @@ enum ib_gid_type {
 	IB_GID_TYPE_SIZE
 };
 
+#define ROCE_V2_UDP_DPORT	1021
+
 struct ib_gid_attr {
 	enum ib_gid_type	gid_type;
 	struct net_device	*ndev;
@@ -722,7 +724,6 @@ struct ib_ah_attr {
 	u8			ah_flags;
 	u8			port_num;
 	u8			dmac[ETH_ALEN];
-	u16			vlan_id;
 };
 
 enum ib_wc_status {
@@ -1095,10 +1096,6 @@ struct ib_qp_attr {
 	u8			rnr_retry;
 	u8			alt_port_num;
 	u8			alt_timeout;
-	u8			smac[ETH_ALEN];
-	u8			alt_smac[ETH_ALEN];
-	u16			vlan_id;
-	u16			alt_vlan_id;
 	u64			dct_key;
 };
 
