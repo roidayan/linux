e45b1f9ccb29c8a3e08cb8ded4bf8bf595e441f9
Alaa Hleihel <alaa@mellanox.com>
2015-02-25 12:57:49 +0200

    compat/compat-3.18.c: define ELFCORE_ADDR_MAX if missing.
    ELFCORE_ADDR_MAX is not defined in SLES12 xen flavor.
    
    issue: none
    Change-Id: If642a92729e4024ebab92bf3bfcede76fddeaed6
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>

--- compat/compat-3.18.c
+++ compat/compat-3.18.c
@@ -1,5 +1,9 @@
 #include <linux/kernel.h>
 #include <linux/crash_dump.h>
 
+#ifndef ELFCORE_ADDR_MAX
+#define ELFCORE_ADDR_MAX        (-1ULL)
+#endif
+
 unsigned long long elfcorehdr_addr = ELFCORE_ADDR_MAX;
 EXPORT_SYMBOL_GPL(elfcorehdr_addr);
