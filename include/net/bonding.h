da190d3d9dd08f1a576f479e132812656050ded8
Alaa Hleihel <alaa@mellanox.com>
2015-02-26 15:02:27 +0200

    include/net/bonding.h: don't call bond_uses_primary
    use macro instead of calling bond_uses_primary
    
    issue: none
    Change-Id: I557bb9d35aa542e3ff853c1d045ed871e709ff85
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>

--- include/net/bonding.h
+++ include/net/bonding.h
@@ -4,13 +4,18 @@
 #ifdef HAVE_BONDING_H
 #include_next <net/bonding.h>
 
+#define MLX_USES_PRIMARY(mode)				\
+		(((mode) == BOND_MODE_ACTIVEBACKUP) ||	\
+		 ((mode) == BOND_MODE_TLB)          ||	\
+		 ((mode) == BOND_MODE_ALB))
+
 #define bond_option_active_slave_get_rcu LINUX_BACKPORT(bond_option_active_slave_get_rcu)
 static inline struct net_device *bond_option_active_slave_get_rcu(struct bonding
 								  *bond)
 {
 	struct slave *slave = rcu_dereference(bond->curr_active_slave);
 
-	return bond_uses_primary(bond) && slave ? slave->dev : NULL;
+	return MLX_USES_PRIMARY(bond->params.mode) && slave ? slave->dev : NULL;
 }
 #endif /* HAVE_BONDING_H */
 
