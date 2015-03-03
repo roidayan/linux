c0a6f25c35e6060ccb59888bab20e10ecdaa8a18
Moni Shoua <monis@mellanox.com>
2015-03-03 12:13:47 +0200

    Revert "IB/mlx4: Change supported RoCE versions"
    This reverts commit 9ca1d99dd2783e6b7d02327b77c2df4ccfd763d1.
    
    Next patch will implement choosing roce_mode according to device support
    and user preference
    
    issue: none
    Change-Id: I4db63c2e3c55f13730c9ac8580d12d93633a9498
    Signed-off-by: Moni Shoua <monis@mellanox.com>

--- drivers/infiniband/hw/mlx4/main.c
+++ drivers/infiniband/hw/mlx4/main.c
@@ -420,10 +420,8 @@ static int eth_link_query_port(struct ib_device *ibdev, u8 port,
 
 	if (mdev->dev->caps.flags & MLX4_DEV_CAP_FLAG_IBOE)
 		props->port_cap_flags	|= IB_PORT_ROCE;
-	if (mdev->dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V2)
-		props->port_cap_flags	|= IB_PORT_ROCE_V2;
-	if (mdev->dev->caps.flags & MLX4_DEV_CAP_FLAG_ROCE_V1_5)
-		props->port_cap_flags	|= IB_PORT_ROCE_V1_5;
+	if (mdev->dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V1_V2)
+		props->port_cap_flags	|= IB_PORT_ROCE_V2 | IB_PORT_ROCE;
 
 	props->gid_tbl_len	= mdev->dev->caps.gid_table_len[port];
 	props->max_msg_sz	= mdev->dev->caps.max_msg_sz;
@@ -1866,6 +1864,9 @@ static int mlx4_ib_update_gids(struct gid_entry *gids,
 			       struct mlx4_ib_dev *ibdev,
 			       u8 port_num)
 {
+	if (ibdev->dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V1_V2)
+		return mlx4_ib_update_gids_v1_v2(gids, ibdev, port_num);
+
 	return mlx4_ib_update_gids_v1(gids, ibdev, port_num);
 }
 
@@ -2644,7 +2645,7 @@ static void *mlx4_ib_add(struct mlx4_dev *dev)
 		goto err_mad;
 
 	if (dev->caps.flags & MLX4_DEV_CAP_FLAG_IBOE ||
-	    dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V2) {
+	    dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V1_V2) {
 		if (!iboe->nb.notifier_call) {
 			iboe->nb.notifier_call = mlx4_ib_netdev_event;
 			err = register_netdevice_notifier(&iboe->nb);
@@ -2654,7 +2655,7 @@ static void *mlx4_ib_add(struct mlx4_dev *dev)
 			}
 		}
 		if (!mlx4_is_slave(dev) &&
-		    dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V2) {
+		    dev->caps.flags2 & MLX4_DEV_CAP_FLAG2_ROCE_V1_V2) {
 			err = mlx4_config_roce_v2_port(dev, ROCE_V2_UDP_DPORT);
 			if (err) {
 				goto err_notif;
