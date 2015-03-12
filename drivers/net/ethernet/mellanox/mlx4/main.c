792e3c5f95a62e016c6ae59d2fb2d5e57a80c4fb
Moni Shoua <monis@mellanox.com>
2015-03-12 09:41:23 +0200

    net/mlx4: Change name of modparam roce_mode_prefer_routable
    Change the name to roce_backward_compatibility_mode for clarity.
    The logic of the param is now reveresed.
    
    issue: none
    Change-Id: Ie1e25c41d38717ea8a954c5cd3e1d2f2e41f9bb0
    Signed-off-by: Moni Shoua <monis@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx4/main.c
+++ drivers/net/ethernet/mellanox/mlx4/main.c
@@ -121,23 +121,23 @@ struct param_data {
 	struct mlx4_dbdf2val_lst	dbdf2val;
 };
 
-static struct param_data roce_mode_prefer_routable = {
+static struct param_data roce_backward_compatibility_mode = {
 	.id		= ROCE_MODE,
 	.dbdf2val = {
-		.name		= "roce_mode param",
+		.name		= "roce_backward_compatibility_mode param",
 		.num_vals	= 1,
-		.def_val	= {1},
+		.def_val	= {0},
 		.range		= {0, 1},
 		.num_inval_vals = 0
 	}
 };
-module_param_string(roce_mode_prefer_routable, roce_mode_prefer_routable.dbdf2val.str,
-		    sizeof(roce_mode_prefer_routable.dbdf2val.str), 0444);
-MODULE_PARM_DESC(roce_mode_prefer_routable,
+module_param_string(roce_backward_compatibility_mode, roce_backward_compatibility_mode.dbdf2val.str,
+		    sizeof(roce_backward_compatibility_mode.dbdf2val.str), 0444);
+MODULE_PARM_DESC(roce_backward_compatibility_mode,
 		 "Set RoCE modes supported by the port\n"
 		 "\tA single value (e.g. 1) to define uniform preferred RoCE_mode value for all devices\n"
 		 "\t\tor a string to map device function numbers to their RoCE mode value (e.g. '0000:04:00.0-0,002b:1c:0b.a-1').\n"
-		 "\t\tAllowed values are 1 for prefer routable (default),  0 for prefer non routable");
+		 "\t\tAllowed values are 1 for be compatible or 0 for don't (default)");
 
 static struct param_data ud_gid_type = {
 	.id		= UD_GID_TYPE,
@@ -2522,16 +2522,16 @@ static void choose_steering_mode(struct mlx4_dev *dev,
 static void choose_roce_mode(struct mlx4_dev *dev,
 			     struct mlx4_dev_cap *dev_cap)
 {
-	int pref_routable;
+	int pref_v1;
 	int gid_type;
 	enum mlx4_set_roce_mode set_roce_mode =
 		MLX4_ROCE_NOT_SUPPORTED;
 	enum mlx4_set_roce_mode
 		supported_roce_mode[2]; /* supported modes for ud_gid_type */
 
-	mlx4_get_val(roce_mode_prefer_routable.dbdf2val.tbl, pci_physfn(dev->persist->pdev), 0, &pref_routable);
+	mlx4_get_val(roce_backward_compatibility_mode.dbdf2val.tbl, pci_physfn(dev->persist->pdev), 0, &pref_v1);
 
-	if (pref_routable) {
+	if (!pref_v1) {
 		if (dev_cap->flags2 & MLX4_DEV_CAP_FLAG2_ROCEV2)
 			set_roce_mode = MLX4_SET_ROCE_MODE_1_5_PLUS_2;
 		else if (dev_cap->flags & MLX4_DEV_CAP_FLAG_R_ROCE)
@@ -4728,9 +4728,9 @@ static int __init mlx4_verify_params(void)
 		return -1;
 	}
 
-	status = update_defaults(&roce_mode_prefer_routable);
+	status = update_defaults(&roce_backward_compatibility_mode);
 	if (status == INVALID_STR) {
-		if (mlx4_fill_dbdf2val_tbl(&roce_mode_prefer_routable.dbdf2val))
+		if (mlx4_fill_dbdf2val_tbl(&roce_backward_compatibility_mode.dbdf2val))
 			return -1;
 	} else if (status == INVALID_DATA) {
 		return -1;
