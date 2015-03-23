4b89cef089258151bdf8cf2e53823ee1f679e452
Ido Shamay <idos@mellanox.com>
2015-03-23 15:01:27 +0200

    net/mlx4_en: Call register_netdevice at the end of init_netdev
    issue: 522061
    
    netdevice Registration should be performed at the end of the driver
    initilization flow, since after calling register_netdevice device
    callbacks may be called, before final configuration of the device.
    
    Example (VXLAN configuration race):
    mlx4_SET_PORT_VXLAN was listed after the register_netdev command.
    System network scripts may configure the interface (UP) right after
    the registration, which also attach unicast VXLAN rule, before
    mlx4_SET_PORT_VXLAN was called, causing the rule attachment to fail.
    
    Change-Id: Iba5daa20ee24ed775944ede386a13381c88ecaed
    Signed-off-by: Ido Shamay <idos@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx4/en_netdev.c
+++ drivers/net/ethernet/mellanox/mlx4/en_netdev.c
@@ -2828,13 +2828,6 @@ int mlx4_en_init_netdev(struct mlx4_en_dev *mdev, int port,
 	netif_carrier_off(dev);
 	mlx4_en_set_default_moderation(priv);
 
-	err = register_netdev(dev);
-	if (err) {
-		en_err(priv, "Netdev registration failed for port %d\n", port);
-		goto out;
-	}
-	priv->registered = 1;
-
 	en_warn(priv, "Using %d TX rings\n", prof->tx_ring_num);
 	en_warn(priv, "Using %d RX rings\n", prof->rx_ring_num);
 
@@ -2882,6 +2875,13 @@ int mlx4_en_init_netdev(struct mlx4_en_dev *mdev, int port,
 		queue_delayed_work(mdev->workqueue, &priv->service_task,
 				   SERVICE_TASK_DELAY);
 
+	err = register_netdev(dev);
+	if (err) {
+		en_err(priv, "Netdev registration failed for port %d\n", port);
+		goto out;
+	}
+
+	priv->registered = 1;
 	return 0;
 
 out:
