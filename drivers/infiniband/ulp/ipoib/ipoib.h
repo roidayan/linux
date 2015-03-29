0fd92436b33ba171dca485c6488ff7594f7a8c0e
Erez Shitrit <erezsh@mellanox.com>
2015-03-29 21:39:23 +0300

    IB/ipoib: Use atomic tx_outstanding to handle TX queue [ib-next:ipoib-s1]
    TX ring handles its state against the kernel via the tx_outstanding
    variable in order to close/open the net queue.
    
    In addition, TX state is handled from two contexts, one is the send from
    UD mode, and the second is from the send of the CM.
    
    Change-Id: I13c5ef1a9af79c9e456d972e0760aad043f81532

--- drivers/infiniband/ulp/ipoib/ipoib.h
+++ drivers/infiniband/ulp/ipoib/ipoib.h
@@ -359,7 +359,7 @@ struct ipoib_dev_priv {
 	unsigned	     tx_tail;
 	struct ib_sge	     tx_sge[MAX_SKB_FRAGS + 1];
 	struct ib_send_wr    tx_wr;
-	unsigned	     tx_outstanding;
+	atomic_t		tx_outstanding;
 	struct ib_wc	     send_wc[MAX_SEND_CQE];
 
 	struct ib_recv_wr    rx_wr;
