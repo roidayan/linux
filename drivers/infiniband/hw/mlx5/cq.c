2aa196816656424549b58118fa20f09d367d8101
Majd Dibbiny <majd@mellanox.com>
2015-04-02 14:20:45 +0300

    IB/mlx5: Add missing code for fast indirect memory reg
    issue: none
    Change-Id: Ie3d097dfdf5df6cd83aa0dd746ffd4ce52e2d515
    Signed-off-by: Majd Dibbiny <majd@mellanox.com>

--- drivers/infiniband/hw/mlx5/cq.c
+++ drivers/infiniband/hw/mlx5/cq.c
@@ -111,6 +111,8 @@ static enum ib_wc_opcode get_umr_comp(struct mlx5_ib_wq *wq, int idx)
 	case IB_WR_FAST_REG_MR:
 		return IB_WC_FAST_REG_MR;
 
+	case IB_WR_REG_INDIR_MR:
+		return IB_WC_REG_INDIR_MR;
 	default:
 		pr_warn("unknown completion status\n");
 		return 0;
