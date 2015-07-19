7ed604ffebb03be1d67cfb1c8551090e11e2baf2
Matan Barak <matanb@mellanox.com>
2015-07-19 16:21:34 +0300

    IB/core: Support RoCE v1.5
    Add RoCE v1.5 port and GID type in IB/core.
    
    Change-Id: I6f26386ac342e3b6783040255cc29b57c5123a10
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -78,6 +78,7 @@ enum ib_gid_type {
 	IB_GID_TYPE_IB        = 0,
 	IB_GID_TYPE_ROCE      = 0,
 	IB_GID_TYPE_ROCE_UDP_ENCAP = 1,
+	IB_GID_TYPE_ROCE_IP_ENCAP = 2,
 	IB_GID_TYPE_SIZE
 };
 
@@ -120,16 +121,6 @@ enum rdma_network_type {
 	RDMA_NETWORK_IPV4,
 	RDMA_NETWORK_IPV6
 };
-
-static inline enum ib_gid_type ib_network_to_gid_type(enum rdma_network_type network_type)
-{
-	if (network_type == RDMA_NETWORK_IPV4 ||
-	    network_type == RDMA_NETWORK_IPV6)
-		return IB_GID_TYPE_ROCE_UDP_ENCAP;
-
-	/* IB_GID_TYPE_IB same as RDMA_NETWORK_ROCE_V1 */
-	return IB_GID_TYPE_IB;
-}
 
 static inline enum rdma_network_type ib_gid_to_network_type(enum ib_gid_type gid_type,
 							    union ib_gid *gid)
@@ -465,6 +456,7 @@ union rdma_protocol_stats {
 #define RDMA_CORE_CAP_PROT_ROCE         0x00200000
 #define RDMA_CORE_CAP_PROT_IWARP        0x00400000
 #define RDMA_CORE_CAP_PROT_ROCE_UDP_ENCAP 0x00800000
+#define RDMA_CORE_CAP_PROT_ROCE_IP_ENCAP 0x01000000
 
 #define RDMA_CORE_PORT_IBA_IB          (RDMA_CORE_CAP_PROT_IB  \
 					| RDMA_CORE_CAP_IB_MAD \
@@ -479,6 +471,12 @@ union rdma_protocol_stats {
 					| RDMA_CORE_CAP_ETH_AH)
 #define RDMA_CORE_PORT_IBA_ROCE_UDP_ENCAP			\
 					(RDMA_CORE_CAP_PROT_ROCE_UDP_ENCAP \
+					| RDMA_CORE_CAP_IB_MAD  \
+					| RDMA_CORE_CAP_IB_CM   \
+					| RDMA_CORE_CAP_AF_IB   \
+					| RDMA_CORE_CAP_ETH_AH)
+#define RDMA_CORE_PORT_IBA_ROCE_IP_ENCAP			\
+					(RDMA_CORE_CAP_PROT_ROCE_IP_ENCAP \
 					| RDMA_CORE_CAP_IB_MAD  \
 					| RDMA_CORE_CAP_IB_CM   \
 					| RDMA_CORE_CAP_AF_IB   \
@@ -2039,12 +2037,18 @@ static inline bool rdma_protocol_ib(const struct ib_device *device, u8 port_num)
 static inline bool rdma_protocol_roce(const struct ib_device *device, u8 port_num)
 {
 	return device->port_immutable[port_num].core_cap_flags &
-		(RDMA_CORE_CAP_PROT_ROCE | RDMA_CORE_CAP_PROT_ROCE_UDP_ENCAP);
+		(RDMA_CORE_CAP_PROT_ROCE | RDMA_CORE_CAP_PROT_ROCE_UDP_ENCAP |
+		 RDMA_CORE_CAP_PROT_ROCE_IP_ENCAP);
 }
 
 static inline bool rdma_protocol_roce_udp_encap(const struct ib_device *device, u8 port_num)
 {
 	return device->port_immutable[port_num].core_cap_flags & RDMA_CORE_CAP_PROT_ROCE_UDP_ENCAP;
+}
+
+static inline bool rdma_protocol_roce_ip_encap(const struct ib_device *device, u8 port_num)
+{
+	return device->port_immutable[port_num].core_cap_flags & RDMA_CORE_CAP_PROT_ROCE_IP_ENCAP;
 }
 
 static inline bool rdma_protocol_roce_eth_encap(const struct ib_device *device, u8 port_num)
@@ -2061,6 +2065,32 @@ static inline bool rdma_ib_or_roce(const struct ib_device *device, u8 port_num)
 {
 	return rdma_protocol_ib(device, port_num) ||
 		rdma_protocol_roce(device, port_num);
+}
+
+static inline enum ib_gid_type ib_network_to_gid_type(const struct ib_device *device,
+						      u8 port_num,
+						      enum rdma_network_type network_type,
+						      union rdma_network_hdr *hdr)
+{
+	if (network_type == RDMA_NETWORK_IPV4 ||
+	    network_type == RDMA_NETWORK_IPV6) {
+		if (hdr) {
+			const struct iphdr *ip4h = (struct iphdr *)&hdr->roce4grh;
+			const struct ipv6hdr *ip6h = (struct ipv6hdr *)&hdr->ibgrh;
+			u8 next_header = (network_type == RDMA_NETWORK_IPV4) ?
+				ip4h->protocol : ip6h->nexthdr;
+
+			return next_header == IPPROTO_UDP ?
+				IB_GID_TYPE_ROCE_UDP_ENCAP :
+				IB_GID_TYPE_ROCE_IP_ENCAP;
+		} else {
+			return min(IB_GID_TYPE_ROCE_UDP_ENCAP,
+				   IB_GID_TYPE_ROCE_IP_ENCAP);
+		}
+	}
+
+	/* IB_GID_TYPE_IB same as RDMA_NETWORK_ROCE_V1 */
+	return IB_GID_TYPE_IB;
 }
 
 /**
