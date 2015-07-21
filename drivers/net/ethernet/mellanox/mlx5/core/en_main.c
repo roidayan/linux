9bea3dd52dca125b88266f4e7dd59c6bf8bc0752
Achiad Shochat <achiad@mellanox.com>
2015-07-22 00:05:25 +0300

    net/mlx5e: Take advantage of the new ndo_stop flow
    Now that TIRs, TISs and flow tables are kept alive after ndo_stop(),
    we can do the following improvements:
    
    -- Obsolete the active_vlans SW shadow.
    -- Do not delete/add flow table rules upon ndo_stop/open.
       In addition to simplifying the flow, this change also fastens
       the ndo_open/close operations.
    -- Obsolete synchronization of threads accessing the flow tables
       with the ndo_stop/open threads.
    
    Change-Id: Ied51fcde386f3714378bfb8f9aa7966a065d3280
    Signed-off-by: Achiad Shochat <achiad@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_main.c
@@ -1374,26 +1374,14 @@ int mlx5e_open_locked(struct net_device *netdev)
 		return err;
 	}
 
-	err = mlx5e_add_all_vlan_rules(priv);
-	if (err) {
-		netdev_err(netdev, "%s: mlx5e_add_all_vlan_rules failed, %d\n",
-			   __func__, err);
-		goto err_close_channels;
-	}
-
 	mlx5e_update_carrier(priv);
 	mlx5e_redirect_rqts(priv);
 	mlx5e_modify_tir_lro(priv, MLX5E_TT_IPV4_TCP);
 	mlx5e_modify_tir_lro(priv, MLX5E_TT_IPV6_TCP);
-	mlx5e_set_rx_mode_core(priv);
 
 	schedule_delayed_work(&priv->update_stats_work, 0);
+
 	return 0;
-
-err_close_channels:
-	mlx5e_close_channels(priv);
-
-	return err;
 }
 
 static int mlx5e_open(struct net_device *netdev)
@@ -1415,8 +1403,6 @@ int mlx5e_close_locked(struct net_device *netdev)
 	clear_bit(MLX5E_STATE_OPENED, &priv->state);
 
 	mlx5e_redirect_rqts(priv);
-	mlx5e_set_rx_mode_core(priv);
-	mlx5e_del_all_vlan_rules(priv);
 	netif_carrier_off(priv->netdev);
 	mlx5e_close_channels(priv);
 
@@ -1836,14 +1822,14 @@ static int mlx5e_set_features(struct net_device *netdev,
 			err = mlx5e_open_locked(priv->netdev);
 	}
 
+	mutex_unlock(&priv->state_lock);
+
 	if (changes & NETIF_F_HW_VLAN_CTAG_FILTER) {
 		if (features & NETIF_F_HW_VLAN_CTAG_FILTER)
 			mlx5e_enable_vlan_filter(priv);
 		else
 			mlx5e_disable_vlan_filter(priv);
 	}
-
-	mutex_unlock(&priv->state_lock);
 
 	return 0;
 }
@@ -2137,6 +2123,7 @@ static void *mlx5e_create_netdev(struct mlx5_core_dev *mdev)
 	}
 
 	mlx5e_enable_async_events(priv);
+	schedule_work(&priv->set_rx_mode_work);
 
 	return priv;
 
@@ -2181,6 +2168,9 @@ static void mlx5e_destroy_netdev(struct mlx5_core_dev *mdev, void *vpriv)
 	struct mlx5e_priv *priv = vpriv;
 	struct net_device *netdev = priv->netdev;
 
+	set_bit(MLX5E_STATE_DESTROYING, &priv->state);
+
+	schedule_work(&priv->set_rx_mode_work);
 	mlx5e_disable_async_events(priv);
 	flush_scheduled_work();
 	unregister_netdev(netdev);
