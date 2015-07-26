947efe48860139878505c876db7d0f9bdf764466
Rana Shahout <ranas@mellanox.com>
2015-07-26 12:22:37 +0300

    net/mlx5_core: Wake On LAN support
    Implement set/get WOL by ethtool.
    
    To get wol - type:
    	ethtool <ethX>
    To set wol - type:
    	ethtool -s <ethX> wol <wol mode: p|u|m|b|a|g|s|d>
    
    issue: none
    
    Change-Id: Ie8d32c67148fe95880c89973eda6d5dd8a77a2da
    Signed-off-by: Rana Shahout <ranas@mellanox.com>

--- include/linux/mlx5/port.h
+++ include/linux/mlx5/port.h
@@ -81,4 +81,7 @@ int mlx5_query_port_tc_group(struct mlx5_core_dev *mdev, u8 *tc_group);
 int mlx5_set_port_tc_bw_alloc(struct mlx5_core_dev *mdev, u8 *tc_bw);
 int mlx5_query_port_tc_bw_alloc(struct mlx5_core_dev *mdev, u8 *tc_bw);
 
+int mlx5_set_port_wol(struct mlx5_core_dev *mdev, u8 wol_mode);
+int mlx5_query_port_wol(struct mlx5_core_dev *mdev, u8 *wol_mode);
+
 #endif /* __MLX5_PORT_H__ */
