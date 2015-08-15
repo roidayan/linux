1830b3379174651d77b080ff35d923a3f4b6c1c7
Saeed Mahameed <saeedm@mellanox.com>
2015-08-15 03:32:37 +0300

    net/mlx5: Introduce access functions to modify/query vport promisc mode
    Those functions are needed to notify the upcoming SR-IOV
    E-Switch(FDB) manager(PF), of the NIC vport (vf) promisc mode changes.
    
    Preperation for ethernet sriov and l2 table management.
    
    issue: 650127
    Change-Id: Iaf75347f48d32702affd563c1d41afe87f9b165d
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/linux/mlx5/vport.h
+++ include/linux/mlx5/vport.h
@@ -73,6 +73,15 @@ int mlx5_modify_nic_vport_mac_list(struct mlx5_core_dev *dev,
 				   enum mlx5_list_type list_type,
 				   u8 addr_list[][ETH_ALEN],
 				   int list_size);
+int mlx5_query_nic_vport_promisc(struct mlx5_core_dev *mdev,
+				 u32 vport,
+				 int *promisc_uc,
+				 int *promisc_mc,
+				 int *promisc_all);
+int mlx5_modify_nic_vport_promisc(struct mlx5_core_dev *mdev,
+				  int promisc_uc,
+				  int promisc_mc,
+				  int promisc_all);
 
 int mlx5_nic_vport_enable_roce(struct mlx5_core_dev *mdev);
 int mlx5_nic_vport_disable_roce(struct mlx5_core_dev *mdev);
