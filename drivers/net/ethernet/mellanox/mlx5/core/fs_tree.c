3d1ebcac8d6cf61db68c00a33f304d260a931a72
Maor Gottlieb <maorg@mellanox.com>
2015-09-10 15:24:15 +0300

    net/mlx5_core: Add flow steering algorithms
    This patch add the following algorithms:
    1) Find next and previous flow table in prio.
    2) Calculate level, when flow table is created.
    3) Match between macth criterias.
    
    Change-Id: Ia458ec0b468a2945ed17e445d07e63cbc5899907
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/fs_tree.c
+++ drivers/net/ethernet/mellanox/mlx5/core/fs_tree.c
@@ -140,3 +140,294 @@ static void fs_remove_node_parent_locked(struct fs_base *node)
 	wait_for_completion(&node->complete);
 	kfree(node);
 }
+
+static struct fs_prio *find_prio(struct mlx5_flow_namespace *ns,
+				 unsigned int prio)
+{
+	struct fs_prio *iter_prio;
+
+	fs_for_each_prio(iter_prio, ns) {
+		if (iter_prio->prio == prio)
+			return iter_prio;
+	}
+
+	return NULL;
+}
+
+static unsigned int _alloc_new_level(struct fs_prio *prio,
+				     struct mlx5_flow_namespace *match);
+
+static unsigned int __alloc_new_level(struct mlx5_flow_namespace *ns,
+				      struct fs_prio *prio)
+{
+	unsigned int level = 0;
+	struct fs_prio *p;
+
+	if (!ns)
+		return 0;
+
+	mutex_lock(&ns->base.lock);
+	fs_for_each_prio(p, ns) {
+		if (p != prio)
+			level += p->max_ft;
+		else
+			break;
+	}
+	mutex_unlock(&ns->base.lock);
+
+	fs_get_parent(prio, ns);
+	if (prio)
+		WARN_ON(prio->base.type != FS_TYPE_PRIO);
+
+	return level + _alloc_new_level(prio, ns);
+}
+
+/* Called under lock of priority, hence locking all upper objects */
+static unsigned int _alloc_new_level(struct fs_prio *prio,
+				     struct mlx5_flow_namespace *match)
+{
+	struct mlx5_flow_namespace *ns;
+	struct fs_base *it;
+	unsigned int level = 0;
+
+	if (!prio)
+		return 0;
+
+	mutex_lock(&prio->base.lock);
+	fs_for_each_ns_or_ft_reverse(it, prio) {
+		if (it->type == FS_TYPE_NAMESPACE) {
+			struct fs_prio *p;
+
+			fs_get_obj(ns, it);
+
+			if (match != ns) {
+				mutex_lock(&ns->base.lock);
+				fs_for_each_prio(p, ns)
+					level += p->max_ft;
+				mutex_unlock(&ns->base.lock);
+			} else {
+				break;
+			}
+		} else {
+			struct mlx5_flow_table *ft;
+
+			fs_get_obj(ft, it);
+			mutex_unlock(&prio->base.lock);
+			return level + ft->level + 1;
+		}
+	}
+
+	fs_get_parent(ns, prio);
+	mutex_unlock(&prio->base.lock);
+	return __alloc_new_level(ns, prio) + level;
+}
+
+static unsigned int alloc_new_level(struct fs_prio *prio)
+{
+	return _alloc_new_level(prio, NULL);
+}
+
+static bool fs_match_exact_mask(u8 match_criteria_enable1,
+				u8 match_criteria_enable2,
+				void *mask1, void *mask2)
+{
+	return match_criteria_enable1 == match_criteria_enable2 &&
+		!memcmp(mask1, mask2, MLX5_ST_SZ_BYTES(fte_match_param));
+}
+
+static struct mlx5_flow_table *find_first_ft_in_ns_reverse(struct mlx5_flow_namespace *ns,
+							   struct list_head *start);
+
+static struct mlx5_flow_table *_find_first_ft_in_prio_reverse(struct fs_prio *prio,
+							      struct list_head *start)
+{
+	struct fs_base *it = container_of(start, struct fs_base, list);
+
+	if (!prio)
+		return NULL;
+
+	fs_for_each_ns_or_ft_continue_reverse(it, prio) {
+		struct mlx5_flow_namespace	*ns;
+		struct mlx5_flow_table		*ft;
+
+		if (it->type == FS_TYPE_FLOW_TABLE) {
+			fs_get_obj(ft, it);
+			fs_get(&ft->base);
+			return ft;
+		}
+
+		fs_get_obj(ns, it);
+		WARN_ON(ns->base.type != FS_TYPE_NAMESPACE);
+
+		ft = find_first_ft_in_ns_reverse(ns, &ns->prios);
+		if (ft)
+			return ft;
+	}
+
+	return NULL;
+}
+
+static struct mlx5_flow_table *find_first_ft_in_prio_reverse(struct fs_prio *prio,
+							     struct list_head *start)
+{
+	struct mlx5_flow_table *ft;
+
+	if (!prio)
+		return NULL;
+
+	mutex_lock(&prio->base.lock);
+	ft = _find_first_ft_in_prio_reverse(prio, start);
+	mutex_unlock(&prio->base.lock);
+
+	return ft;
+}
+
+static struct mlx5_flow_table *find_first_ft_in_ns_reverse(struct mlx5_flow_namespace *ns,
+							   struct list_head *start)
+{
+	struct fs_prio *prio;
+
+	if (!ns)
+		return NULL;
+
+	fs_get_obj(prio, container_of(start, struct fs_base, list));
+	mutex_lock(&ns->base.lock);
+	fs_for_each_prio_continue_reverse(prio, ns) {
+		struct mlx5_flow_table *ft;
+
+		ft = find_first_ft_in_prio_reverse(prio, &prio->objs);
+		if (ft) {
+			mutex_unlock(&ns->base.lock);
+			return ft;
+		}
+	}
+	mutex_unlock(&ns->base.lock);
+
+	return NULL;
+}
+
+/* Returned a held ft, assumed curr is protected, assumed curr's parent is
+ * locked
+ */
+static struct mlx5_flow_table *find_prev_ft(struct mlx5_flow_table *curr,
+					    struct fs_prio *prio)
+{
+	struct mlx5_flow_table *ft = NULL;
+	struct fs_base *curr_base;
+
+	if (!curr)
+		return NULL;
+
+	/* prio has either namespace or flow-tables, but not both */
+	if (!list_empty(&prio->objs) &&
+	    list_first_entry(&prio->objs, struct mlx5_flow_table, base.list) !=
+	    curr)
+		return NULL;
+
+	while (!ft && prio) {
+		struct mlx5_flow_namespace *ns;
+
+		fs_get_parent(ns, prio);
+		ft = find_first_ft_in_ns_reverse(ns, &prio->base.list);
+		curr_base = &ns->base;
+		fs_get_parent(prio, ns);
+
+		if (prio && !ft)
+			ft = find_first_ft_in_prio_reverse(prio,
+							   &curr_base->list);
+	}
+	return ft;
+}
+
+static struct mlx5_flow_table *find_first_ft_in_ns(struct mlx5_flow_namespace *ns,
+						   struct list_head *start);
+
+static struct mlx5_flow_table *_find_first_ft_in_prio(struct fs_prio *prio,
+						      struct list_head *start)
+{
+	struct fs_base	*it = container_of(start, struct fs_base, list);
+
+	if (!prio)
+		return NULL;
+
+	fs_for_each_ns_or_ft_continue(it, prio) {
+		struct mlx5_flow_namespace	*ns;
+		struct mlx5_flow_table		*ft;
+
+		if (it->type == FS_TYPE_FLOW_TABLE) {
+			fs_get_obj(ft, it);
+			fs_get(&ft->base);
+			return ft;
+		}
+
+		fs_get_obj(ns, it);
+		WARN_ON(ns->base.type != FS_TYPE_NAMESPACE);
+
+		ft = find_first_ft_in_ns(ns, &ns->prios);
+		if (ft)
+			return ft;
+	}
+
+	return NULL;
+}
+
+static struct mlx5_flow_table *find_first_ft_in_prio(struct fs_prio *prio,
+						     struct list_head *start)
+{
+	struct mlx5_flow_table *ft;
+
+	if (!prio)
+		return NULL;
+
+	mutex_lock(&prio->base.lock);
+	ft = _find_first_ft_in_prio(prio, start);
+	mutex_unlock(&prio->base.lock);
+
+	return ft;
+}
+
+static struct mlx5_flow_table *find_first_ft_in_ns(struct mlx5_flow_namespace *ns,
+						   struct list_head *start)
+{
+	struct fs_prio *prio;
+
+	if (!ns)
+		return NULL;
+
+	fs_get_obj(prio, container_of(start, struct fs_base, list));
+	mutex_lock(&ns->base.lock);
+	fs_for_each_prio_continue(prio, ns) {
+		struct mlx5_flow_table *ft;
+
+		ft = find_first_ft_in_prio(prio, &prio->objs);
+		if (ft) {
+			mutex_unlock(&ns->base.lock);
+			return ft;
+		}
+	}
+	mutex_unlock(&ns->base.lock);
+
+	return NULL;
+}
+
+/* returned a held ft, assumed curr is protected, assumed curr's parent is
+ * locked
+ */
+static struct mlx5_flow_table *find_next_ft(struct fs_prio *prio)
+{
+	struct mlx5_flow_table *ft = NULL;
+	struct fs_base *curr_base;
+
+	while (!ft && prio) {
+		struct mlx5_flow_namespace *ns;
+
+		fs_get_parent(ns, prio);
+		ft = find_first_ft_in_ns(ns, &prio->base.list);
+		curr_base = &ns->base;
+		fs_get_parent(prio, ns);
+
+		if (!ft && prio)
+			ft = _find_first_ft_in_prio(prio, &curr_base->list);
+	}
+	return ft;
+}
