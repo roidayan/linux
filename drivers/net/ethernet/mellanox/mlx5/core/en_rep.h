60a6cf2922973daa7da6430a179d6c962bb57200
Or Gerlitz <ogerlitz@mellanox.com>
2015-10-07 14:39:06 +0200

    mlx5e: Add en_rep.h

---
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015, Mellanox Technologies. All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#define MLX5_REP_HW_ID_LEN 6
+
+struct mlx5e_vf_rep {
+	struct net_device *dev;
+	struct mlx5e_priv *pf_dev;
+	struct mlx5e_sq *reinject_sq;
+
+	u8  hw_id[MLX5_REP_HW_ID_LEN];
+	// u8  vf;
+	u32 vport;
+	u32 miss_flow_index;
+
+	u32 vf_mac_flow_index; /* FIXME - support multiple MACs --> flow indexes */
+};
