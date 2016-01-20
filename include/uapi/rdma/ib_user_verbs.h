43efdd5302f5c2015b088a2454940d5b0f3f62c5
Leon Romanovsky <leonro@mellanox.com>
2016-01-20 17:18:27 +0200

    IB/core: Add offload arithmetic operations support
    The offload arithmetic operations support allows to the user to
    benefit from the parallel calculation of several input vectors of
    equal length.
    
    The newly added flag indicates this ability.
    
    The old bitwise device_cap_flags variable was limited to u32
    which has all bits already defined. In order to overcome it,
    we added new u64 device_cap_flags2 variable which can be
    accessible by ib_uverbs_ex_query_device command.
    
    Issue: 688855
    Change-Id: I10bfc3adeb4a1206b6ef20f440d63726c963e7ea
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- include/uapi/rdma/ib_user_verbs.h
+++ include/uapi/rdma/ib_user_verbs.h
@@ -231,6 +231,11 @@ struct ib_uverbs_ex_query_device_resp {
 	struct ib_uverbs_odp_caps odp_caps;
 	__u64 timestamp_mask;
 	__u64 hca_core_clock; /* in KHZ */
+	/*
+	 * Original capability field is limited
+	 * by __u32 variable.  Need to increase this field
+	 */
+	__u64 device_cap_flags2;
 };
 
 struct ib_uverbs_query_port {
