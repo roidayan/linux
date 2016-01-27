2e2ad4def4f752288604f2642f76bda6870fb2fd
Maor Gottlieb <maorg@mellanox.com>
2016-01-27 18:08:02 +0200

    net/mlx5_en: create aRFS flow tables
    Creates the following four flow tables for aRFS usage:
    1. IPv4 TCP - for filtering 4-tuple of IPv4 tcp packets.
    2. IPv6 TCP - for filtering 4-tuple of IPv6 tcp packets.
    3. IPv4 UDP - for filtering 4-tuple of IPv4 udp packets.
    4. IPv6 UDP - for filtering 4-tuple of IPv6 udp packets.
    
    Each flow table has two groups: one for the 4-tuple filtering
    and the other contains * rule for packet that doesn't have
    match on the 4-tuple rules, this rule steer the packet to the
    relevant RSS TIR.
    
    Issue: 693406
    Change-Id: I822e764f6f53ad9c8425738d7cde28ec1007aeeb
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_fs.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_fs.c
@@ -45,7 +45,8 @@ static void mlx5e_del_l2_flow_rule(struct mlx5e_priv *priv,
 enum {
 	MLX5E_VLAN_FT_LEVEL = 0,
 	MLX5E_L2_FT_LEVEL,
-	MLX5E_TTC_FT_LEVEL
+	MLX5E_TTC_FT_LEVEL,
+	MLX5E_ARFS_FT_LEVEL
 };
 
 #define MLX5_SET_CFG(p, f, v) MLX5_SET(create_flow_group_in, p, f, v)
@@ -535,6 +536,219 @@ static void mlx5e_destroy_flow_table(struct mlx5e_flow_table *ft)
 	kfree(ft->g);
 	mlx5_destroy_flow_table(ft->t);
 	ft->t = NULL;
+}
+
+static void mlx5e_destroy_arfs_table(struct mlx5e_arfs_table *arfs_t)
+{
+	mlx5_del_flow_rule(arfs_t->default_rule);
+	mlx5e_destroy_flow_table(&arfs_t->ft);
+}
+
+static void mlx5e_destroy_arfs_flow_tables(struct mlx5e_priv *priv)
+{
+	int i;
+
+	for (i = 0; i < MLX5E_ARFS_NUM_TYPES; i++) {
+		if (!IS_ERR_OR_NULL(priv->fs.arfs.arfs_tables[i].ft.t))
+			mlx5e_destroy_arfs_table(&priv->fs.arfs.arfs_tables[i]);
+	}
+}
+
+static int mlx5e_add_arfs_default_rule(struct mlx5e_priv *priv,
+				       enum mlx5e_arfs_type type)
+{
+	struct mlx5e_arfs_table *arfs_t = &priv->fs.arfs.arfs_tables[type];
+	struct mlx5_flow_destination dest;
+	u8 match_criteria_enable = 0;
+	u32 *tirn = priv->indir_tirn;
+	u32 *match_criteria;
+	u32 *match_value;
+	int err = 0;
+
+	match_value	= mlx5_vzalloc(MLX5_ST_SZ_BYTES(fte_match_param));
+	match_criteria	= mlx5_vzalloc(MLX5_ST_SZ_BYTES(fte_match_param));
+	if (!match_value || !match_criteria) {
+		netdev_err(priv->netdev, "%s: alloc failed\n", __func__);
+		err = -ENOMEM;
+		goto out;
+	}
+
+	dest.type = MLX5_FLOW_DESTINATION_TYPE_TIR;
+	switch (type) {
+	case MLX5E_ARFS_IPV4_TCP:
+		dest.tir_num = tirn[MLX5E_TT_IPV4_TCP];
+		break;
+	case MLX5E_ARFS_IPV4_UDP:
+		dest.tir_num = tirn[MLX5E_TT_IPV4_UDP];
+		break;
+	case MLX5E_ARFS_IPV6_TCP:
+		dest.tir_num = tirn[MLX5E_TT_IPV6_TCP];
+		break;
+	case MLX5E_ARFS_IPV6_UDP:
+		dest.tir_num = tirn[MLX5E_TT_IPV6_UDP];
+		break;
+	default:
+		err = -EINVAL;
+		goto out;
+	}
+
+	arfs_t->default_rule = mlx5_add_flow_rule(arfs_t->ft.t, match_criteria_enable,
+						  match_criteria, match_value,
+						  MLX5_FLOW_CONTEXT_ACTION_FWD_DEST,
+						  MLX5_FS_DEFAULT_FLOW_TAG,
+						  &dest);
+	if (IS_ERR_OR_NULL(arfs_t->default_rule)) {
+		err = PTR_ERR(arfs_t->default_rule);
+		arfs_t->default_rule = NULL;
+		netdev_err(priv->netdev, "%s: add rule failed, arfs type=%d\n",
+			   __func__, type);
+	}
+out:
+	kvfree(match_criteria);
+	kvfree(match_value);
+	return err;
+}
+
+#define MLX5E_ARFS_NUM_GROUPS	2
+#define MLX5E_ARFS_GROUP1_SIZE	BIT(12)
+#define MLX5E_ARFS_GROUP2_SIZE	BIT(0)
+#define MLX5E_ARFS_TABLE_SIZE	(MLX5E_ARFS_GROUP1_SIZE +\
+				 MLX5E_ARFS_GROUP2_SIZE)
+static int mlx5e_create_arfs_groups(struct mlx5e_flow_table *ft,
+				    enum  mlx5e_arfs_type type)
+{
+	int inlen = MLX5_ST_SZ_BYTES(create_flow_group_in);
+	void *outer_headers_c;
+	int ix = 0;
+	u32 *in;
+	int err;
+	u8 *mc;
+
+	ft->g = kcalloc(MLX5E_ARFS_NUM_GROUPS,
+			sizeof(*ft->g), GFP_KERNEL);
+	if (!ft->g)
+		return -ENOMEM;
+	in = mlx5_vzalloc(inlen);
+	if (!in) {
+		kfree(ft->g);
+		return -ENOMEM;
+	}
+
+	mc = MLX5_ADDR_OF(create_flow_group_in, in, match_criteria);
+	outer_headers_c = MLX5_ADDR_OF(fte_match_param, mc,
+				       outer_headers);
+	MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, ethertype);
+	switch (type) {
+	case MLX5E_ARFS_IPV4_TCP:
+	case MLX5E_ARFS_IPV6_TCP:
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, tcp_dport);
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, tcp_sport);
+		break;
+	case MLX5E_ARFS_IPV4_UDP:
+	case MLX5E_ARFS_IPV6_UDP:
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, udp_dport);
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c, udp_sport);
+		break;
+	default:
+		err = -EINVAL;
+		goto free;
+	}
+
+	switch (type) {
+	case MLX5E_ARFS_IPV4_TCP:
+	case MLX5E_ARFS_IPV4_UDP:
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c,
+				 src_ipv4_src_ipv6.ipv4_layout.ipv4);
+		MLX5_SET_TO_ONES(fte_match_set_lyr_2_4, outer_headers_c,
+				 dst_ipv4_dst_ipv6.ipv4_layout.ipv4);
+		break;
+	case MLX5E_ARFS_IPV6_TCP:
+	case MLX5E_ARFS_IPV6_UDP:
+		memset(MLX5_ADDR_OF(fte_match_set_lyr_2_4, outer_headers_c,
+				    src_ipv4_src_ipv6.ipv6_layout.ipv6),
+		       0xff, 16);
+		memset(MLX5_ADDR_OF(fte_match_set_lyr_2_4, outer_headers_c,
+				    dst_ipv4_dst_ipv6.ipv6_layout.ipv6),
+		       0xff, 16);
+		break;
+	default:
+		err = -EINVAL;
+		goto free;
+	}
+
+	MLX5_SET_CFG(in, match_criteria_enable, MLX5_MATCH_OUTER_HEADERS);
+	MLX5_SET_CFG(in, start_flow_index, ix);
+	ix += MLX5E_ARFS_GROUP1_SIZE;
+	MLX5_SET_CFG(in, end_flow_index, ix - 1);
+	ft->g[ft->num_groups] = mlx5_create_flow_group(ft->t, in);
+	if (IS_ERR(ft->g[ft->num_groups]))
+		goto err;
+	ft->num_groups++;
+
+	memset(in, 0, inlen);
+	MLX5_SET_CFG(in, start_flow_index, ix);
+	ix += MLX5E_ARFS_GROUP2_SIZE;
+	MLX5_SET_CFG(in, end_flow_index, ix - 1);
+	ft->g[ft->num_groups] = mlx5_create_flow_group(ft->t, in);
+	if (IS_ERR(ft->g[ft->num_groups]))
+		goto err;
+	ft->num_groups++;
+
+	kvfree(in);
+	return 0;
+
+err:
+	err = PTR_ERR(ft->g[ft->num_groups]);
+	ft->g[ft->num_groups] = NULL;
+free:
+	kvfree(in);
+
+	return err;
+}
+
+static int mlx5e_create_arfs_flow_table(struct mlx5e_priv *priv,
+					enum mlx5e_arfs_type type)
+{
+	struct mlx5e_arfs *arfs = &priv->fs.arfs;
+	struct mlx5e_flow_table *ft = &arfs->arfs_tables[type].ft;
+	int err;
+
+	ft->t = mlx5_create_flow_table(priv->fs.ns, 0, MLX5E_ARFS_TABLE_SIZE,
+				       MLX5E_ARFS_FT_LEVEL);
+	if (IS_ERR(ft->t)) {
+		err = PTR_ERR(ft->t);
+		ft->t = NULL;
+		return err;
+	}
+
+	err = mlx5e_create_arfs_groups(ft, type);
+	if (err)
+		goto err;
+
+	err = mlx5e_add_arfs_default_rule(priv, type);
+	if (err)
+		goto err;
+
+	return 0;
+err:
+	mlx5e_destroy_flow_table(ft);
+	return err;
+}
+
+static int mlx5e_create_arfs_flow_tables(struct mlx5e_priv *priv)
+{
+	int err = 0;
+	int i;
+
+	for (i = 0; i < MLX5E_ARFS_NUM_TYPES; i++) {
+		err = mlx5e_create_arfs_flow_table(priv, i);
+		if (err)
+			goto err;
+	}
+	return 0;
+err:
+	mlx5e_destroy_arfs_flow_tables(priv);
+	return err;
 }
 
 static void mlx5e_clean_ttc_rules(struct mlx5e_ttc *ttc)
@@ -1080,9 +1294,13 @@ int mlx5e_create_flow_tables(struct mlx5e_priv *priv)
 	if (!priv->fs.ns)
 		return -EINVAL;
 
-	err = mlx5e_create_ttc_flow_table(priv);
+	err = mlx5e_create_arfs_flow_tables(priv);
 	if (err)
 		return err;
+
+	err = mlx5e_create_ttc_flow_table(priv);
+	if (err)
+		goto err_destroy_arfs_flow_tables;
 
 	err = mlx5e_create_l2_flow_table(priv);
 	if (err)
@@ -1098,6 +1316,8 @@ err_destroy_l2_flow_table:
 	mlx5e_destroy_l2_flow_table(priv);
 err_destroy_ttc_flow_table:
 	mlx5e_destroy_ttc_flow_table(priv);
+err_destroy_arfs_flow_tables:
+	mlx5e_destroy_arfs_flow_tables(priv);
 
 	return err;
 }
@@ -1108,4 +1328,5 @@ void mlx5e_destroy_flow_tables(struct mlx5e_priv *priv)
 	mlx5e_destroy_vlan_flow_table(priv);
 	mlx5e_destroy_l2_flow_table(priv);
 	mlx5e_destroy_ttc_flow_table(priv);
+	mlx5e_destroy_arfs_flow_tables(priv);
 }
