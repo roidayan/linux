23b139b5c79db8b867ac4e2fec9d5149f8004db9
Tariq Toukan <tariqt@mellanox.com>
2016-02-07 16:18:01 +0200

    net/mlx5: Implement alloc, dealloc and query queue counters
    Queue counters collect some statistics about QP.
    
    For netdev mlx5_core preserve MLX5_NUM_VPORT_Q_COUNTER_SETS_FOR_NETDEV
    sets of vport queue counters. (default value is 64), IB will take the rest.
    
    Issue: None
    Change-Id: I06dd63c4b793fc8375aceb6ecda165d6e371b254
    Signed-off-by: Rana Shahout <ranas@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/qp.c
+++ drivers/net/ethernet/mellanox/mlx5/core/qp.c
@@ -538,3 +538,128 @@ void mlx5_core_destroy_sq_tracked(struct mlx5_core_dev *dev,
 	mlx5_core_destroy_sq(dev, sq->qpn);
 }
 EXPORT_SYMBOL(mlx5_core_destroy_sq_tracked);
+
+int mlx5_core_alloc_q_counter(struct mlx5_core_dev *mdev,
+			      int intf_id, u16 *counter_set_id)
+{
+	u32 in[MLX5_ST_SZ_DW(alloc_q_counter_in)];
+	u32 out[MLX5_ST_SZ_DW(alloc_q_counter_out)];
+	struct mlx5_q_counter_allocator *q_counter;
+	int err;
+
+	if (intf_id < 0 || intf_id >= MLX5_NUM_INTERFACE) {
+		mlx5_core_warn(mdev, "Unknown intrface id: %d\n", intf_id);
+		return -EINVAL;
+	}
+	q_counter = &mdev->priv.q_counter_allocator[intf_id];
+
+	mutex_lock(&q_counter->lock);
+	if (q_counter->used == q_counter->max) {
+		err = -ENOSPC;
+		goto unlock;
+	}
+
+	memset(in, 0, sizeof(in));
+	memset(out, 0, sizeof(out));
+
+	MLX5_SET(alloc_q_counter_in, in, opcode,
+		 MLX5_CMD_OP_ALLOC_Q_COUNTER);
+
+	err = mlx5_cmd_exec_check_status(mdev, in, sizeof(in),
+					 out, sizeof(out));
+
+	if (!err) {
+		*counter_set_id = MLX5_GET(alloc_q_counter_out, out,
+					   counter_set_id);
+		q_counter->used++;
+	}
+
+unlock:
+	mutex_unlock(&q_counter->lock);
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(mlx5_core_alloc_q_counter);
+
+int mlx5_core_dealloc_q_counter(struct mlx5_core_dev *mdev,
+				int intf_id, u16 counter_set_id)
+{
+	u32 in[MLX5_ST_SZ_DW(dealloc_q_counter_in)];
+	u32 out[MLX5_ST_SZ_DW(dealloc_q_counter_out)];
+	struct mlx5_q_counter_allocator *q_counter;
+	int err;
+
+	if (intf_id < 0 || intf_id >= MLX5_NUM_INTERFACE) {
+		mlx5_core_warn(mdev, "Unknown intrface id: %d\n", intf_id);
+		return -EINVAL;
+	}
+	q_counter = &mdev->priv.q_counter_allocator[intf_id];
+
+	mutex_lock(&q_counter->lock);
+	if (q_counter->used == 0) {
+		err = -EINVAL;
+		goto unlock;
+	}
+
+	memset(in, 0, sizeof(in));
+	memset(out, 0, sizeof(out));
+
+	MLX5_SET(dealloc_q_counter_in, in, opcode,
+		 MLX5_CMD_OP_DEALLOC_Q_COUNTER);
+	MLX5_SET(dealloc_q_counter_in, in, counter_set_id,
+		 counter_set_id);
+
+	err = mlx5_cmd_exec_check_status(mdev, in, sizeof(in),
+					 out, sizeof(out));
+
+	if (!err)
+		q_counter->used--;
+
+unlock:
+	mutex_unlock(&q_counter->lock);
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(mlx5_core_dealloc_q_counter);
+
+int mlx5_core_query_q_counter(struct mlx5_core_dev *mdev,
+			      u16 counter_set_id,
+			      int reset,
+			      void *out,
+			      int out_size)
+{
+	u32 in[MLX5_ST_SZ_DW(query_q_counter_in)];
+	int err;
+
+	memset(in, 0, sizeof(in));
+
+	MLX5_SET(query_q_counter_in, in, opcode, MLX5_CMD_OP_QUERY_Q_COUNTER);
+	MLX5_SET(query_q_counter_in, in, clear, reset);
+	MLX5_SET(query_q_counter_in, in, counter_set_id, counter_set_id);
+
+	err = mlx5_cmd_exec_check_status(mdev, in, sizeof(in),
+					 out, out_size);
+
+	return err;
+}
+EXPORT_SYMBOL_GPL(mlx5_core_query_q_counter);
+
+int mlx5_core_query_out_of_buffer(struct mlx5_core_dev *mdev,
+				  u16 counter_set_id,
+				  u32 *out_of_buffer)
+{
+	int outlen = MLX5_ST_SZ_BYTES(query_q_counter_out);
+	void *out;
+	int err;
+
+	out = mlx5_vzalloc(outlen);
+
+	err = mlx5_core_query_q_counter(mdev, counter_set_id, 0, out,
+					outlen);
+
+	if (!err)
+		*out_of_buffer = MLX5_GET(query_q_counter_out, out,
+					  out_of_buffer);
+	kfree(out);
+	return err;
+}
