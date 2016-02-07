e629d52836d775d83556d50261041bb2c580ca26
Tariq Toukan <tariqt@mellanox.com>
2016-02-07 16:57:19 +0200

    net/mlx5e: Support ECN
    As part of supporting ECN:
    - Implement FW commands of controling congestion
    - Implement sysfs for user to control ECN configuration.
    
    User can control ECN configurable parameters through sysfs
    structure:
    	/sys/class/net/<Interface>/
    
    User can query ecn enable per priority X:
    	   cat /sys/class/net/<interface>/ecn/<protocol>/enable/X
    User can read ecn configurable parameters:
    	cat  /sys/class/net/<interface>/ecn/<protocol>/requested attributes
    User can enabled ecn for each priority per protocol:
    	   echo 1 >  /sys/class/net/<interface>/ecn/<protocol>/enable/X
    User can modify ecn configurable parameters:
    	echo <value> >  /sys/class/net/<interface>/ecn/<protocol>/requested attributes
    
    Where:
    	X: priority {0..7}
    	protocol: roce_rp / roce_np
    
    issue: none
    
    Change-Id: Id703f014c881334605e6bf7fc3efd4fe2dae1c8a
    Signed-off-by: Rana Shahout <ranas@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

--- include/linux/mlx5/port.h
+++ include/linux/mlx5/port.h
@@ -83,5 +83,12 @@ int mlx5_query_port_ets_rate_limit(struct mlx5_core_dev *mdev,
 				   u8 *max_bw_unit);
 int mlx5_set_port_wol(struct mlx5_core_dev *mdev, u8 wol_mode);
 int mlx5_query_port_wol(struct mlx5_core_dev *mdev, u8 *wol_mode);
-
+int mlx5_query_port_cong_status(struct mlx5_core_dev *mdev, int protocol,
+				int priority, int *is_enable);
+int mlx5_modify_port_cong_status(struct mlx5_core_dev *mdev, int protocol,
+				 int priority, int enable);
+int mlx5_query_port_cong_params(struct mlx5_core_dev *mdev, int protocol,
+				void *out, int out_size);
+int mlx5_modify_port_cong_params(struct mlx5_core_dev *mdev,
+				 void *in, int in_size);
 #endif /* __MLX5_PORT_H__ */
