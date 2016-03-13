1193c148fd2f8cc792edbde21d48b30d23280fd0
Eran Ben Elisha <eranbe@mellanox.com>
2016-03-13 14:47:00 +0200

    net/mlx5e: Disable link up on INIT HCA command
    Disable link up when initializing the HCA. Link up/down will be changed
    using (Ports Administrative and Operational Status Register) PAOS
    commands.
    
    If link layer is Ethernet, up/down the link in ndo_open/stop.  If link
    layer is IB, up/down the link as part of the mlx5 IB add/remove flow.
    
    Before this patch the link was up when the HCA was initialized, now the
    driver will manage the link.
    
    Issue:749575
    Change-Id: Idf1c85a5d29c396ee73c6bb8f98342d5ff001d0d
    Signed-off-by: Eran Ben Elisha <eranbe@mellanox.com>
    Signed-off-by: Eugenia Emantayev <eugenia@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -756,7 +756,8 @@ struct mlx5_ifc_cmd_hca_cap_bits {
 	u8         local_ca_ack_delay[0x5];
 	u8         reserved_at_1af[0x2];
 	u8         ports_check[0x1];
-	u8         reserved_at_1b2[0x2];
+	u8         reserved_at_1b2[0x1];
+	u8         disable_link_up[0x1];
 	u8         beacon_led[0x1];
 	u8         port_type[0x2];
 	u8         num_ports[0x8];
