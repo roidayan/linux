a66fb0c111a59a43b6fe38c54e7d7824b14b0e6a
Aviv Heller <avivh@mellanox.com>
2016-05-09 11:06:44 +0000

    net/mlx5_core: Get RoCE netdev
    Used by IB driver for determining the IB bond
    device's netdev, when LAG is active.
    
    Returns PF0's netdev if mode is not active-backup,
    or the PF netdev of the active slave when mode is
    active-backup.
    
    Issue: 801557
    Change-Id: Ie728eb3ba419f5c7f7af578e5014e2b2272698d5
    Signed-off-by: Aviv Heller <avivh@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/lag.c
+++ drivers/net/ethernet/mellanox/mlx5/core/lag.c
@@ -528,3 +528,26 @@ bool mlx5_lag_is_active(struct mlx5_core_dev *dev)
 }
 EXPORT_SYMBOL(mlx5_lag_is_active);
 
+struct net_device *mlx5_lag_get_roce_netdev(struct mlx5_core_dev *dev)
+{
+	struct net_device *ndev = NULL;
+	struct lag_dev *ldev;
+
+	mutex_lock(&lag_mutex);
+	ldev = mlx5_lag_dev_get(dev);
+	if (ldev && mlx5_lag_is_bonded(ldev)) {
+		if (ldev->tracker.tx_type == NETDEV_LAG_TX_TYPE_ACTIVEBACKUP) {
+			ndev = ldev->tracker.netdev_state[0].tx_enabled ?
+			       ldev->pf[0].netdev : ldev->pf[1].netdev;
+		} else {
+			ndev = ldev->pf[0].netdev;
+		}
+		if (ndev)
+			dev_hold(ndev);
+	}
+	mutex_unlock(&lag_mutex);
+
+	return ndev;
+}
+EXPORT_SYMBOL(mlx5_lag_get_roce_netdev);
+
