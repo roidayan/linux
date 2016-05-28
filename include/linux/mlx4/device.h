91652dae56da797fbab3a5e457fa50d6747ac55b
Moshe Shemesh <moshe@mellanox.com>
2016-05-28 11:11:22 +0300

    net/mlx4_core: Add an option to configure SVLAN TPID
    Expose mlx4_config_svlan_tpid, that checks for feature support
    in device capabilities and sets the Service Tag TPID accordingly.
    
    issue: 769060
    Change-Id: I39d8284f321bdea2ca1ea6ac7d27233b95e3b948
    Signed-off-by: Moshe Shemesh <moshe@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

--- include/linux/mlx4/device.h
+++ include/linux/mlx4/device.h
@@ -221,6 +221,7 @@ enum {
 	MLX4_DEV_CAP_FLAG2_ROCE_V1_V2		= 1ULL <<  33,
 	MLX4_DEV_CAP_FLAG2_DMFS_UC_MC_SNIFFER   = 1ULL <<  34,
 	MLX4_DEV_CAP_FLAG2_SVLAN_BY_QP          = 1ULL <<  35,
+	MLX4_DEV_CAP_FLAG2_SVLAN_TPID		= 1ULL <<  36,
 };
 
 enum {
@@ -1476,6 +1477,7 @@ int mlx4_get_base_gid_ix(struct mlx4_dev *dev, int slave, int port);
 int mlx4_config_vxlan_port(struct mlx4_dev *dev, __be16 udp_port);
 int mlx4_disable_rx_port_check(struct mlx4_dev *dev, bool dis);
 int mlx4_config_roce_v2_port(struct mlx4_dev *dev, u16 udp_port);
+int mlx4_config_svlan_tpid(struct mlx4_dev *dev, __be16 svlan_tpid);
 int mlx4_virt2phy_port_map(struct mlx4_dev *dev, u32 port1, u32 port2);
 int mlx4_vf_smi_enabled(struct mlx4_dev *dev, int slave, int port);
 int mlx4_vf_get_enable_smi_admin(struct mlx4_dev *dev, int slave, int port);
