5659d3ba0efe6554c3b49df81ea151d33c1fbc2f
Gal Pressman <galp@mellanox.com>
2016-09-28 09:59:11 +0300

    net/mlx5e: Add support for ethtool get actual speed callback
    ethtool DEVNAME will now show actual port speed.
    
    Signed-off-by: Gal Pressman <galp@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/lib/vport.h
+++ drivers/net/ethernet/mellanox/mlx5/lib/vport.h
@@ -43,6 +43,8 @@ enum {
 };
 
 u8 mlx5_query_vport_state(struct mlx5_core_dev *mdev, u8 opmod, u16 vport);
+u16 mlx5_query_vport_max_tx_speed(struct mlx5_core_dev *mdev, u8 opmod,
+				  u16 vport);
 u8 mlx5_query_vport_admin_state(struct mlx5_core_dev *mdev, u8 opmod,
 				u16 vport);
 int mlx5_modify_vport_admin_state(struct mlx5_core_dev *mdev, u8 opmod,
