ef3b3ce8b6336b4c165cc1b86407fb7f9e399449
Roi Dayan <roid@mellanox.com>
2016-12-28 09:24:15 +0200

    mlx5e: Fix invalid access to flow attributes
    Flows on the uplink are not removed when switching between modes.
    So flow allocated in legacy mode will not have eswitch attributes so need
    to verify that if we request to delete a flow in offloads mode.
    
    Issue: 935342
    Change-Id: I82992bd45ee993fd8389c87abfd80e0a9d515ca6
    Signed-off-by: Roi Dayan <roid@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -181,7 +181,11 @@ static void mlx5e_tc_del_flow(struct mlx5e_priv *priv,
 		mlx5_fc_destroy(priv->mdev, counter);
 	}
 
-	if (esw && esw->mode == SRIOV_OFFLOADS) {
+	if (esw && esw->mode == SRIOV_OFFLOADS && flow->attr) {
+		/**
+		 * flow->attr is null for flows allocated when esw was in
+		 * legacy mode.
+		 **/
 		mlx5_eswitch_del_vlan_action(esw, flow->attr);
 		if (flow->attr->action & MLX5_FLOW_CONTEXT_ACTION_ENCAP)
 			mlx5e_detach_encap(priv, flow);
@@ -1218,7 +1222,6 @@ int mlx5e_delete_flower(struct mlx5e_priv *priv,
 	rhashtable_remove_fast(&tc->ht, &flow->node, tc->ht_params);
 
 	mlx5e_tc_del_flow(priv, flow);
-
 
 	kfree(flow);
 
