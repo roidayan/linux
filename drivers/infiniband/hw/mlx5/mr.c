4173210ce5a2b1b7ad431d0668d4c1a0d542ab5b
Ilya Lesokhin <ilyal@mellanox.com>
2017-04-04 12:58:52 +0300

    IB/mlx5: ODP MRs must be UMRable
    Currently reg_create creates non UMRable MRs, so they won't work
    with ODP. To fix this we change reg_create to create UMRable MRs.
    
    We further split the concepts of UMR and MR cache.
     * Rename MAX_UMR_CACHE_ENTRY to be MR_CACHE_LAST_MTT_ENTRY.
     * Remove the mlx5_ib_mr->umred bit and instead rely on the existence
       of mlx5_ib_dev->umem and the MR order to decide if the MR should be
       returned to the cache or destroyed using destroy_mkey.
    
    Signed-off-by: Ilya Lesokhin <ilyal@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/mlx5/mr.c
+++ drivers/infiniband/hw/mlx5/mr.c
@@ -47,9 +47,22 @@ enum {
 
 #define MLX5_UMR_ALIGN 2048
 
+static int release_mr(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr);
 static int clean_mr(struct mlx5_ib_mr *mr);
 static int use_umr(struct mlx5_ib_dev *dev, int order);
 static int unreg_umr(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr);
+
+static int mr_cache_max_order(struct mlx5_ib_dev *dev)
+{
+	if (MLX5_CAP_GEN(dev->mdev, umr_extended_translation_offset))
+		return MR_CACHE_LAST_MTT_ENTRY + 2;
+	return MLX5_MAX_UMR_SHIFT;
+}
+
+static int return_to_mr_cache(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr)
+{
+	return mr->umem && mr->order <= mr_cache_max_order(dev);
+}
 
 static int destroy_mkey(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr)
 {
@@ -183,7 +196,6 @@ static int add_keys(struct mlx5_ib_dev *dev, int c, int num)
 			break;
 		}
 		mr->order = ent->order;
-		mr->umred = 1;
 		mr->dev = dev;
 
 		MLX5_SET(mkc, mkc, free, 1);
@@ -495,12 +507,12 @@ static struct mlx5_ib_mr *alloc_cached_mr(struct mlx5_ib_dev *dev, int order)
 	int i;
 
 	c = order2idx(dev, order);
-	if (c < 0 || c > MAX_UMR_CACHE_ENTRY) {
+	if (c < 0 || c > MR_CACHE_LAST_MTT_ENTRY) {
 		mlx5_ib_warn(dev, "order %d, cache index %d\n", order, c);
 		return NULL;
 	}
 
-	for (i = c; i < MAX_UMR_CACHE_ENTRY; i++) {
+	for (i = c; i < MR_CACHE_LAST_MTT_ENTRY; i++) {
 		ent = &cache->ent[i];
 
 		mlx5_ib_dbg(dev, "order %d, cache index %d\n", ent->order, i);
@@ -669,7 +681,7 @@ int mlx5_mr_cache_init(struct mlx5_ib_dev *dev)
 		INIT_DELAYED_WORK(&ent->dwork, delayed_cache_work_func);
 		queue_work(cache->wq, &ent->work);
 
-		if (i > MAX_UMR_CACHE_ENTRY) {
+		if (i > MR_CACHE_LAST_MTT_ENTRY) {
 			mlx5_odp_init_mr_cache_entry(ent);
 			continue;
 		}
@@ -808,9 +820,7 @@ static int get_octo_len(u64 addr, u64 len, int page_size)
 
 static int use_umr(struct mlx5_ib_dev *dev, int order)
 {
-	if (MLX5_CAP_GEN(dev->mdev, umr_extended_translation_offset))
-		return order <= MAX_UMR_CACHE_ENTRY + 2;
-	return order <= MLX5_MAX_UMR_SHIFT;
+	return order <= mr_cache_max_order(dev);
 }
 
 static int mr_umem_get(struct ib_pd *pd, u64 start, u64 length,
@@ -1131,6 +1141,7 @@ static struct mlx5_ib_mr *reg_create(struct ib_mr *ibmr, struct ib_pd *pd,
 	MLX5_SET(mkc, mkc, rr, !!(access_flags & IB_ACCESS_REMOTE_READ));
 	MLX5_SET(mkc, mkc, lw, !!(access_flags & IB_ACCESS_LOCAL_WRITE));
 	MLX5_SET(mkc, mkc, lr, 1);
+	MLX5_SET(mkc, mkc, umr_en, 1);
 
 	MLX5_SET64(mkc, mkc, start_addr, virt_addr);
 	MLX5_SET64(mkc, mkc, len, length);
@@ -1337,7 +1348,7 @@ int mlx5_ib_rereg_user_mr(struct ib_mr *ib_mr, int flags, u64 start,
 		/*
 		 * UMR can't be used - MKey needs to be replaced.
 		 */
-		if (mr->umred) {
+		if (return_to_mr_cache(dev, mr)) {
 			err = unreg_umr(dev, mr);
 			if (err)
 				mlx5_ib_warn(dev, "Failed to unregister MR\n");
@@ -1354,8 +1365,6 @@ int mlx5_ib_rereg_user_mr(struct ib_mr *ib_mr, int flags, u64 start,
 
 		if (IS_ERR(mr))
 			return PTR_ERR(mr);
-
-		mr->umred = 0;
 	} else {
 		/*
 		 * Send a UMR WQE
@@ -1440,11 +1449,29 @@ mlx5_free_priv_descs(struct mlx5_ib_mr *mr)
 	}
 }
 
+static int release_mr(struct mlx5_ib_dev *dev, struct mlx5_ib_mr *mr)
+{
+	int err;
+
+	if (return_to_mr_cache(dev, mr)) {
+		mlx5_mr_cache_free(dev, mr);
+		return 0;
+	}
+
+	err = destroy_mkey(dev, mr);
+	if (err) {
+		mlx5_ib_warn(dev, "failed to destroy mkey 0x%x (%d)\n",
+			     mr->mmkey.key, err);
+		return err;
+	}
+	kfree(mr);
+
+	return 0;
+}
+
 static int clean_mr(struct mlx5_ib_mr *mr)
 {
 	struct mlx5_ib_dev *dev = to_mdev(mr->ibmr.device);
-	int umred = mr->umred;
-	int err;
 
 	if (mr->sig) {
 		if (mlx5_core_destroy_psv(dev->mdev,
@@ -1461,21 +1488,7 @@ static int clean_mr(struct mlx5_ib_mr *mr)
 
 	mlx5_free_priv_descs(mr);
 
-	if (!umred) {
-		err = destroy_mkey(dev, mr);
-		if (err) {
-			mlx5_ib_warn(dev, "failed to destroy mkey 0x%x (%d)\n",
-				     mr->mmkey.key, err);
-			return err;
-		}
-	} else {
-		mlx5_mr_cache_free(dev, mr);
-	}
-
-	if (!umred)
-		kfree(mr);
-
-	return 0;
+	return release_mr(dev, mr);
 }
 
 int mlx5_ib_dereg_mr(struct ib_mr *ibmr)
