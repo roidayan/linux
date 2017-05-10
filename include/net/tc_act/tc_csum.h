627539c188363d3af0d05622f150f3743f67e610
Or Gerlitz <ogerlitz@mellanox.com>
2017-05-10 13:48:41 +0300

    net/sched: act_csum: Add basic accessor
    Add the very basic accessor for just realizing if this
    is a csum action.
    
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Reviewed-by: Paul Blakey <paulb@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/net/tc_act/tc_csum.h
+++ include/net/tc_act/tc_csum.h
@@ -3,6 +3,7 @@
 
 #include <linux/types.h>
 #include <net/act_api.h>
+#include <linux/tc_act/tc_csum.h>
 
 struct tcf_csum {
 	struct tc_action common;
@@ -10,5 +11,14 @@ struct tcf_csum {
 	u32 update_flags;
 };
 #define to_tcf_csum(a) ((struct tcf_csum *)a)
+
+static inline bool is_tcf_csum(const struct tc_action *a)
+{
+#ifdef CONFIG_NET_CLS_ACT
+	if (a->ops && a->ops->type == TCA_ACT_CSUM)
+		return true;
+#endif
+	return false;
+}
 
 #endif /* __NET_TC_CSUM_H */
