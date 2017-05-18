0c1838315f5ca2edf4de93af6860c5895e40da9d
Mohamad Haj Yahia <mohamad@mellanox.com>
2017-05-18 17:48:08 +0300

    net: Add SRIOV VGT+ support
    VGT+ is a security feature that gives the administrator the ability of
    controlling the allowed vlan-ids list that can be transmitted/received
    from/to the VF.
    The allowed vlan-ids list is called "trunk".
    Admin can add/delete a range of allowed vlan-ids via iptool.
    
    Example:
    After this series of configuration :
    1) ip link set eth3 vf 0 trunk add 10 100 (allow vlan-id 10-100)
    2) ip link set eth3 vf 0 trunk add 105 (allow vlan-id 105)
    3) ip link set eth3 vf 0 trunk rem 90 (block vlan-id 90)
    4) ip link set eth3 vf 0 trunk rem 50 60 (block vlan-ids 50-60)
    The VF 0 can only communicate on vlan-ids: 10-49,61-89,91-100,105.
    
    For this purpose we added the following netlink sr-iov commands:
    1) IFLA_VF_VLAN_RANGE: used to add/remove allowed vlan-ids range.
    we added the ifla_vf_vlan_range struct to specify the range we want to
    add/remove from the userspace.
    we added ndo_add_vf_vlan_trunk_range and ndo_del_vf_vlan_trunk_range
    netdev ops to add/remove allowed vlan-ids range in the netdev.
    2) IFLA_VF_VLAN_TRUNK: used to query the allowed vlan-ids trunk.
    we added trunk bitmap to the ifla_vf_info struct to get the current
    allowed vlan-ids trunk from the netdev.
    we added ifla_vf_vlan_trunk struct for sending the allowed vlan-ids
    trunk to the userspace.
    
    Signed-off-by: Mohamad Haj Yahia <mohamad@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/linux/if_link.h
+++ include/linux/if_link.h
@@ -25,6 +25,7 @@ struct ifla_vf_info {
 	__u32 max_tx_rate;
 	__u32 rss_query_en;
 	__u32 trusted;
+	unsigned long trunk[BITS_TO_LONGS(VF_VLAN_N_VID)];
 	__be16 vlan_proto;
 };
 #endif /* _LINUX_IF_LINK_H */
