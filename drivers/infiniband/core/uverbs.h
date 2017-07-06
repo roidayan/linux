ed088d331cd439c7caefc701a91b7b782188dd98
Raed Salem <raeds@mellanox.com>
2017-07-06 11:57:33 +0300

    IB/uverbs: Add support for flow counter set
    The struct ib_uverbs_flow_spec_action_count associates
    a counter set with the flow.
    
    Use of ib_uverbs_flow_spec_action_count allows the
    consumer to associate an existing counter set to
    a flow on creation.
    
    Signed-off-by: Raed Salem <raeds@mellanox.com>
    Reviewed-by: Yishai Hadas <yishaih@mellanox.com>

--- drivers/infiniband/core/uverbs.h
+++ drivers/infiniband/core/uverbs.h
@@ -244,6 +244,7 @@ struct ib_uverbs_flow_spec {
 		struct ib_uverbs_flow_spec_ipv6    ipv6;
 		struct ib_uverbs_flow_spec_action_tag	flow_tag;
 		struct ib_uverbs_flow_spec_action_drop	drop;
+		struct ib_uverbs_flow_spec_action_count flow_count;
 	};
 };
 
