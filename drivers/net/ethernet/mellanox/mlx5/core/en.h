3b52479d14d4fa005c51abe4fd1e73de021fe39c
Saeed Mahameed <saeedm@mellanox.com>
2017-07-27 14:47:50 +0300

    net/mlx5e: XDP TX MPWQE no inline copy
    This patch provides the option to setup XDP TX SQs to not copy packet
    header to WQE inline segment when multi_pkt_send_wqe capability is
    supported by HW.
    
    multi_pkt_send_wqe is supported in ConnectX4-LX and later cards.
    The feature is disabled by default and can be turned on/off via new priv
    flag.
    
    ethtool --show-priv-flags eth20
    Private flags for eth20:
    rx_cqe_moder   : on
    rx_cqe_compress: off
    xdp_tx_mpwqe   : off
    
    ethtool --set-priv-flags eth20 xdp_tx_mpwqe on
    ethtool --set-priv-flags eth20 xdp_tx_mpwqe off
    
    From initial testing On a single XDP TX flow with xdp_tx_mpwqe turned on
    we see a 2%-4% packet rate improvement.
    
    The only downside about enabling this feature in ConnectX4-LX is that
    XDP TX packet will only be forwarded to wire and will not loopback to
    device. So for XDP programs that are meant to forward packets to the
    same device (inter VF/PF traffic) (Eswitch or Multihost) this
    configuration will not work.
    
    Issue: 894740
    Change-Id: I12508f660c4f2b43f4ccdc6c3475f65919c57693
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Tariq Toukan <tariqt@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -207,6 +207,7 @@ static const char mlx5e_priv_flags[][ETH_GSTRING_LEN] = {
 	"tx_cqe_moder",
 	"rx_cqe_compress",
 	"rx_striding_rq",
+	"xdp_tx_mpwqe",
 };
 
 enum mlx5e_priv_flag {
@@ -214,6 +215,7 @@ enum mlx5e_priv_flag {
 	MLX5E_PFLAG_TX_CQE_BASED_MODER = (1 << 1),
 	MLX5E_PFLAG_RX_CQE_COMPRESS = (1 << 2),
 	MLX5E_PFLAG_RX_STRIDING_RQ = (1 << 3),
+	MLX5E_PFLAG_XDP_TX_MPWQE = (1 << 4)
 };
 
 #define MLX5E_SET_PFLAG(params, pflag, enable)			\
