189964826465f24557a46c2c86673ab6e1056c39
Leon Romanovsky <leon@kernel.org>
2017-08-14 09:47:37 +0300

    RDMA/mlx5: Limit scope of get vector affinity local function
    The mlx5_ib_get_vector_affinity() call is local to main.c file and there
    is no need to be declared globally visible.
    
    Fixes: 40b24403f33e ("mlx5: support ->get_vector_affinity")
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -3818,8 +3818,8 @@ static void init_delay_drop(struct mlx5_ib_dev *dev)
 		mlx5_ib_warn(dev, "Failed to init delay drop debugfs\n");
 }
 
-const struct cpumask *mlx5_ib_get_vector_affinity(struct ib_device *ibdev,
-		int comp_vector)
+static const struct cpumask *
+mlx5_ib_get_vector_affinity(struct ib_device *ibdev, int comp_vector)
 {
 	struct mlx5_ib_dev *dev = to_mdev(ibdev);
 
