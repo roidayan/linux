96b14b2f674baa2eadf5704a5dd59216f3ed450e
Boris Pismenny <borisp@mellanox.com>
2017-09-04 14:57:03 +0300

    IB/mlx5: Add IPsec support for egress and ingress
    This commit introduces support for the esp_aes_gcm flow
    specification for the Innova device. To that end we add
    two new root namespace tables to represent the Innova IPsec
    tables: MLX5_FLOW_NAMESPACE_IPSEC_TX, MLX5_FLOW_NAMESPACE_IPSEC_RX.
    
    Signed-off-by: Matan Barak <matanb@mellanox.com>
    Signed-off-by: Boris Pismenny <borisp@mellanox.com>
    Signed-off-by: Leon Romanovsky <leon@kernel.org>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -436,7 +436,9 @@ struct mlx5_ifc_fte_match_set_misc_bits {
 
 	u8         reserved_at_120[0x28];
 	u8         bth_dst_qp[0x18];
-	u8         reserved_at_160[0xa0];
+	u8	   reserved_at_160[0x20];
+	u8	   outer_esp_spi[0x20];
+	u8         reserved_at_1a0[0x60];
 };
 
 struct mlx5_ifc_cmd_pas_bits {
@@ -1067,6 +1069,7 @@ enum mlx5_flow_destination_type {
 	MLX5_FLOW_DESTINATION_TYPE_VPORT        = 0x0,
 	MLX5_FLOW_DESTINATION_TYPE_FLOW_TABLE   = 0x1,
 	MLX5_FLOW_DESTINATION_TYPE_TIR          = 0x2,
+	MLX5_FLOW_DESTINATION_TYPE_WIRE         = 0x3,
 
 	MLX5_FLOW_DESTINATION_TYPE_COUNTER      = 0x100,
 };
