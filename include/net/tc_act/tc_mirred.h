283ac3473fdc32116b1196be6fd989b29b5de991
Cong Wang <xiyou.wangcong@gmail.com>
2017-12-05 16:17:26 -0800

    act_mirred: get rid of tcfm_ifindex from struct tcf_mirred
    tcfm_dev always points to the correct netdev and we already
    hold a refcnt, so no need to use tcfm_ifindex to lookup again.
    
    If we would support moving target netdev across netns, using
    pointer would be better than ifindex.
    
    This also fixes dumping obsolete ifindex, now after the
    target device is gone we just dump 0 as ifindex.
    
    Cc: Jiri Pirko <jiri@mellanox.com>
    Cc: Jamal Hadi Salim <jhs@mojatatu.com>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Acked-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/net/tc_act/tc_mirred.h
+++ include/net/tc_act/tc_mirred.h
@@ -13,52 +13,6 @@ struct tcf_mirred {
 };
 #define to_mirred(a) ((struct tcf_mirred *)a)
 
-static const struct nla_policy mirred_policy_compat[TCA_MIRRED_MAX + 1] = {
-	[TCA_MIRRED_PARMS]		= { .len = sizeof(struct tc_mirred) },
-};
-
-static inline bool is_tcf_mirred_compat(const struct tc_action *a)
-{
-#ifdef CONFIG_NET_CLS_ACT
-	if (a->ops && a->ops->type == TCA_ACT_MIRRED)
-		return true;
-#endif
-	return false;
-}
-
-static struct tc_mirred to_mirred_compat(const struct tc_action *a)
-{
-	struct nlattr *tb[TCA_MIRRED_MAX + 1];
-	struct tc_mirred m = { .ifindex = 0 };
-	struct sk_buff *skb;
-	struct nlattr *nla;
-
-	if (!a->ops || !a->ops->dump || !is_tcf_mirred_compat(a))
-		return m;
-
-	skb = alloc_skb(256, GFP_KERNEL);
-	if (!skb)
-		return m;
-
-	if (a->ops->dump(skb, (struct tc_action *) a, 0, 0) < 0)
-		goto freeskb;
-
-	nla = (struct nlattr *) skb->data;
-	if (nla_parse(tb, TCA_MIRRED_MAX, nla, skb->len, mirred_policy_compat)
-		      < 0)
-		goto freeskb;
-
-	if (!tb[TCA_MIRRED_PARMS])
-		goto freeskb;
-
-	m = *((struct tc_mirred *) nla_data(tb[TCA_MIRRED_PARMS]));
-
-freeskb:
-	kfree_skb(skb);
-
-	return m;
-}
-
 static inline bool is_tcf_mirred_egress_redirect(const struct tc_action *a)
 {
 #ifdef CONFIG_NET_CLS_ACT
@@ -80,11 +34,6 @@ static inline bool is_tcf_mirred_egress_mirror(const struct tc_action *a)
 static inline struct net_device *tcf_mirred_dev(const struct tc_action *a)
 {
 	return rtnl_dereference(to_mirred(a)->tcfm_dev);
-}
-
-static inline int tcf_mirred_ifindex(const struct tc_action *a)
-{
-	return to_mirred_compat(a).ifindex;
 }
 
 #endif /* __NET_TC_MIR_H */
