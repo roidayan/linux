6eb27c55c050b82d418d74d306537474a5e4e309
Gal Pressman <galp@mellanox.com>
2017-12-25 18:40:52 +0200

    net/mlx5e: Vxlan, check maximum number of UDP ports
    The NIC has a limited number of offloaded VXLAN UDP ports (usually 4).
    Instead of letting the firmware fail when trying to add more ports than
    it can handle, let the driver check it on its own.
    
    Signed-off-by: Gal Pressman <galp@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -683,7 +683,11 @@ struct mlx5_ifc_per_protocol_networking_offload_caps_bits {
 	u8         swp[0x1];
 	u8         swp_csum[0x1];
 	u8         swp_lso[0x1];
-	u8         reserved_at_23[0x1d];
+	u8         reserved_at_23[0xd];
+	u8         max_vxlan_udp_ports[0x8];
+	u8         reserved_at_38[0x6];
+	u8         max_geneve_opt_len[0x1];
+	u8         tunnel_stateless_geneve_rx[0x1];
 
 	u8         reserved_at_40[0x10];
 	u8         lro_min_mss_size[0x10];
