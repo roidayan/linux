6880ffa987ee3ecb19c9988b73b363b885ffd2ce
Feras Daoud <ferasda@mellanox.com>
2018-02-06 15:15:22 +0200

    net/mlx5: FW tracer, add hardware structures
    This change adds the infrastructure to mlx5 core fw tracer.
    It introduces the following 4 new registers:
    MLX5_REG_MTRC_CAP  - Used to read tracer capabilities
    MLX5_REG_MTRC_CONF - Used to set tracer configurations
    MLX5_REG_MTRC_STDB - Used to query tracer strings database
    MLX5_REG_MTRC_CTRL - Used to control the tracer
    
    The capability of the tracing can be checked using mcam access
    register, therefore, the mcam access register interface will expose
    the tracer register.
    
    Initializing the tracer will be part of load one flow, as multiple
    PFs will try to acquire ownership but only one will succeed and will
    be the tracer owner.
    
    Issue: 1177849
    Change-Id: I2690e2fd6d1154b3bc812c85efc66634f41fa89c
    Signed-off-by: Feras Daoud <ferasda@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -138,6 +138,10 @@ enum {
 	MLX5_REG_HOST_ENDIANNESS = 0x7004,
 	MLX5_REG_MCIA		 = 0x9014,
 	MLX5_REG_MLCR		 = 0x902b,
+	MLX5_REG_MTRC_CAP	 = 0x9040,
+	MLX5_REG_MTRC_CONF	 = 0x9041,
+	MLX5_REG_MTRC_STDB	 = 0x9042,
+	MLX5_REG_MTRC_CTRL	 = 0x9043,
 	MLX5_REG_MPCNT		 = 0x9051,
 	MLX5_REG_MTPPS		 = 0x9053,
 	MLX5_REG_MTPPSE		 = 0x9054,
@@ -812,6 +816,7 @@ struct mlx5_clock {
 };
 
 struct mlx5_vxlan;
+struct mlx5_fw_tracer;
 
 struct mlx5_core_dev {
 	struct pci_dev	       *pdev;
@@ -858,6 +863,7 @@ struct mlx5_core_dev {
 	struct mlx5_clock        clock;
 	struct mlx5_ib_clock_info  *clock_info;
 	struct page             *clock_info_page;
+	struct mlx5_fw_tracer   *tracer;
 };
 
 struct mlx5_db {
