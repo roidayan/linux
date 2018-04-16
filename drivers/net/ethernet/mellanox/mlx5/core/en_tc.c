590f19aac681f589d8a6390019cef8ba6a57cc56
Jianbo Liu <jianbol@mellanox.com>
2018-04-16 07:56:53 +0000

    fixup! net/mlx5e: Allow offloading ipv4 header re-write for icmp
    Issue: 1332238
    Change-Id: I7eeaf295cecf28f6027cc6ced94710a266e2b5ce
    Signed-off-by: Jianbo Liu <jianbol@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1809,7 +1809,7 @@ out_err:
 static bool csum_offload_supported(struct mlx5e_priv *priv, u32 action, u32 update_flags)
 {
 	u32 prot_flags = TCA_CSUM_UPDATE_FLAG_IPV4HDR | TCA_CSUM_UPDATE_FLAG_TCP |
-			 TCA_CSUM_UPDATE_FLAG_UDP | TCA_CSUM_UPDATE_FLAG_ICMP;
+			 TCA_CSUM_UPDATE_FLAG_UDP;
 
 	/*  The HW recalcs checksums only if re-writing headers */
 	if (!(action & MLX5_FLOW_CONTEXT_ACTION_MOD_HDR)) {
