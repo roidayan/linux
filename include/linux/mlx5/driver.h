64206d6b8b0b368d7607befdafcdf4edac3f47b1
Saeed Mahameed <saeedm@mellanox.com>
2018-05-09 13:28:00 -0700

    net/mlx5e: Vxlan, move vxlan logic to core driver
    Move vxlan logic and objects to mlx5 core dirver.
    Since it going to be used from different mlx5 interfaces.
    e.g. mlx5e PF NIC netdev and mlx5e E-Switch representors.
    
    Issue: 1248944
    Change-Id: Ibf284ddc8ca12e0fc65d728eb9baee461dfeb7fb
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Reviewed-by: Or Gerlitz <ogerlitz@mellanox.com>

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -811,6 +811,8 @@ struct mlx5_clock {
 	struct mlx5_pps            pps_info;
 };
 
+struct mlx5_vxlan;
+
 struct mlx5_core_dev {
 	struct pci_dev	       *pdev;
 	/* sync pci state */
@@ -842,6 +844,7 @@ struct mlx5_core_dev {
 	atomic_t		num_qps;
 	u32			issi;
 	struct mlx5e_resources  mlx5e_res;
+	struct mlx5_vxlan       *vxlan;
 	struct {
 		struct mlx5_rsvd_gids	reserved_gids;
 		u32			roce_en;
