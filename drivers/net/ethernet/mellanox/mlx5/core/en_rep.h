43a976c48ac7c381ae83dd34e1be126ae6909fca
Vlad Buslov <vladbu@mellanox.com>
2018-06-13 14:49:46 +0300

    net/mlx5e: Extract code that queues neigh update work into function
    As a preparation for next patch, extract the code that queues neigh update
    work, into standalone function. Intention is to use this function in code
    that attaches encap to neigh hash entry in case when neigh was changed
    concurrently during encap initialization.
    
    issue: 1521226
    Change-Id: Ia075c1eb409cca6eae5db98718ac649c75e3f5f9
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rep.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rep.h
@@ -163,6 +163,9 @@ void mlx5e_rep_encap_entry_detach(struct mlx5e_priv *priv,
 				  struct mlx5e_encap_entry *e);
 
 void mlx5e_rep_queue_neigh_stats_work(struct mlx5e_priv *priv);
+bool mlx5e_rep_queue_neigh_update_work(struct mlx5e_priv *priv,
+				       struct mlx5e_neigh_hash_entry *nhe,
+				       struct neighbour *n);
 #else /* CONFIG_MLX5_ESWITCH */
 static inline void mlx5e_register_vport_reps(struct mlx5e_priv *priv) {}
 static inline void mlx5e_unregister_vport_reps(struct mlx5e_priv *priv) {}
