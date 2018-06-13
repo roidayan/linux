0138484b0d8b5a66ca237130c35ad00b18b32c2c
Leon Romanovsky <leonro@mellanox.com>
2018-06-13 11:33:00 +0300

    RDMA/uverbs: Don't overwrite NULL pointer with ZERO_SIZE_PTR
    Number of specs is provided by user and in valid case can be equal to zero.
    Such argument causes to call to kcalloc() with zero-length request and in
    return the ZERO_SIZE_PTR is assigned. This pointer is different from NULL
    and makes various if (..) checks to success, but causes to crashes
    later.
    
    Fixes: b6ba4a9aa59f ("IB/uverbs: Add support for flow counters")
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -2762,6 +2762,9 @@ static struct ib_uflow_resources *flow_resources_alloc(size_t num_specs)
 	if (!resources)
 		return NULL;
 
+	if (!num_specs)
+		goto out;
+
 	resources->counters =
 		kcalloc(num_specs, sizeof(*resources->counters), GFP_KERNEL);
 	resources->collection =
@@ -2770,8 +2773,8 @@ static struct ib_uflow_resources *flow_resources_alloc(size_t num_specs)
 	if (!resources->counters || !resources->collection)
 		goto err;
 
+out:
 	resources->max = num_specs;
-
 	return resources;
 
 err:
