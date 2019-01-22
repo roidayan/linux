99f4adaead21c5afe81feaf44e4911efa81f06ba
Paul Blakey <paulb@mellanox.com>
2019-01-22 18:16:39 +0200

    net/sched: cls_flower: Fix removal of rhashtable entry on error flow
    On error flow from HW and skip_sw, we forgot to remove the rhashtable
    entry we added for the new flow.
    So the flow is freed but still can be found in fl_classify and
    fl_lookup.
    
    Fixes: 38bcc12723fe ('net/sched: cls_flower: Reject duplicated rules also under skip_sw')
    Change-Id: I074b870aaf7a2a781001b1dabfa061319cfad8a0
    Signed-off-by: Paul Blakey <paulb@mellanox.com>

--- net/sched/cls_flower.c
+++ net/sched/cls_flower.c
@@ -1161,7 +1161,7 @@ static int fl_change(struct net *net, struct sk_buff *in_skb,
 	if (!tc_skip_hw(fnew->flags)) {
 		err = fl_hw_replace_filter(tp, fnew);
 		if (err)
-			goto errout_mask;
+			goto errout_remove;
 	}
 
 	if (!tc_in_hw(fnew->flags))
@@ -1192,6 +1192,10 @@ static int fl_change(struct net *net, struct sk_buff *in_skb,
 	kfree(tb);
 	return 0;
 
+errout_remove:
+	rhashtable_remove_fast(&fnew->mask->ht,
+			       &fnew->ht_node,
+			       fnew->mask->filter_ht_params);
 errout_mask:
 	fl_mask_put(head, fnew->mask, false);
 errout_idr:
