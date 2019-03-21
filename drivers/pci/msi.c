4cf383d473864a5768a998829b5663823b40cb57
Chris Mi <chrism@mellanox.com>
2019-03-21 02:26:12 -0400

    Revert "PCI: Provide sensible IRQ vector alloc/free routines"
    This reverts commit 8a1368f31dcb10966961515f0c5a40e39aa6936b.
    
    In this patch, pci_alloc_irq_vectors and pci_free_irq_vectors are
    defined, but not used. Because in order to use them, more patches
    need to be backported that is too complex. So we still use old API.
    But OFED thinks we are using these APIs and we can't compile OFED
    correctly.
    
    Signed-off-by: Chris Mi <chrism@mellanox.com>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -4,7 +4,6 @@
  *
  * Copyright (C) 2003-2004 Intel
  * Copyright (C) Tom Long Nguyen (tom.l.nguyen@intel.com)
- * Copyright (C) 2016 Christoph Hellwig.
  */
 
 #include <linux/err.h>
@@ -1165,60 +1164,6 @@ int pci_enable_msix_range(struct pci_dev *dev, struct msix_entry *entries,
 	return nvec;
 }
 EXPORT_SYMBOL(pci_enable_msix_range);
-
-/**
- * pci_alloc_irq_vectors - allocate multiple IRQs for a device
- * @dev:		PCI device to operate on
- * @min_vecs:		minimum number of vectors required (must be >= 1)
- * @max_vecs:		maximum (desired) number of vectors
- * @flags:		flags or quirks for the allocation
- *
- * Allocate up to @max_vecs interrupt vectors for @dev, using MSI-X or MSI
- * vectors if available, and fall back to a single legacy vector
- * if neither is available.  Return the number of vectors allocated,
- * (which might be smaller than @max_vecs) if successful, or a negative
- * error code on error. If less than @min_vecs interrupt vectors are
- * available for @dev the function will fail with -ENOSPC.
- *
- * To get the Linux IRQ number used for a vector that can be passed to
- * request_irq() use the pci_irq_vector() helper.
- */
-int pci_alloc_irq_vectors(struct pci_dev *dev, unsigned int min_vecs,
-		unsigned int max_vecs, unsigned int flags)
-{
-	int vecs = -ENOSPC;
-
-	if (!(flags & PCI_IRQ_NOMSIX)) {
-		vecs = pci_enable_msix_range(dev, NULL, min_vecs, max_vecs);
-		if (vecs > 0)
-			return vecs;
-	}
-
-	if (!(flags & PCI_IRQ_NOMSI)) {
-		vecs = pci_enable_msi_range(dev, min_vecs, max_vecs);
-		if (vecs > 0)
-			return vecs;
-	}
-
-	/* use legacy irq if allowed */
-	if (!(flags & PCI_IRQ_NOLEGACY) && min_vecs == 1)
-		return 1;
-	return vecs;
-}
-EXPORT_SYMBOL(pci_alloc_irq_vectors);
-
-/**
- * pci_free_irq_vectors - free previously allocated IRQs for a device
- * @dev:		PCI device to operate on
- *
- * Undoes the allocations and enabling in pci_alloc_irq_vectors().
- */
-void pci_free_irq_vectors(struct pci_dev *dev)
-{
-	pci_disable_msix(dev);
-	pci_disable_msi(dev);
-}
-EXPORT_SYMBOL(pci_free_irq_vectors);
 
 /**
  * pci_irq_vector - return Linux IRQ number of a device vector
